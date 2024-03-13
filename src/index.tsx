145b2981fc8089eeba19cf506d51a313431b0b11
Sergey Starchenko <pdxserg@gmail.com>
2024-03-12 21:53:35 -0700

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,68 @@
 import ReactDOM from 'react-dom/client';
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const StyledAnalyst = styled.div`
-    background: url("https://cdn.iconscout.com/icon/free/png-256/free-css3-8-1175200.png") center/cover no-repeat;
+const ModalWindow = styled.div.attrs(() => ({
+	role: "XXX",
+	"aria-YYY": true
+}) )`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    background-color: rgba(0, 0, 0, 0.3);
+    color: #1f1d1d;
+`
 
-    @media (orientation: landscape), (min-width: 1200px) {
-        width: 500px;
-        height: 500px;
-    }
-    @media  (orientation: portrait), (max-width: 768px) {
-        width: 250px;
-        height: 250px;
-    }
-`;
+const ModalContent = styled.div`
+    background-color: white;
+    max-width: 500px;
+    width: 100%;
+    padding: 20px;
+    margin: auto;
+    border-radius: 10px
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    gap: 30px;
+    margin-top: 30px;
+`
+
+const StyledButton = styled.button<{ btnType: string }>`
+    border-radius: 5px;
+    border: 2px solid;
+    cursor: pointer;
+    padding: 15px;
+    flex-grow: 1;
+
+    ${props => props.btnType === "boat" && css`
+        border-color: #2fbc69;
+        background-color: #5de79777;
+    `}
+    
+    ${props => props.btnType === "construction" && css`
+        border-color: #e13c5f;
+        background-color: rgba(241, 94, 126, 0.4)
+    `}
+`
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-	<div className="App">
-		<StyledAnalyst />
-	</div>
+	<ModalWindow>
+		<ModalContent>
+			<h2>Подумай хорошо 🙇‍♂️</h2>
+			<p>Точно хочешь выучить c#?</p>
+			<Wrapper>
+				<StyledButton btnType="construction">Нет ❌</StyledButton>
+				<StyledButton btnType="boat">Да ✔️</StyledButton>
+			</Wrapper>
+		</ModalContent>
+	</ModalWindow>
 );
 
-
-// Какие значения  должны быть вместо XXX, YYY и ZZZ, чтобы для элемента большое изображение было на десктопе или ландшафтной ориентации, а маленькое - на мобилках или портретой ориентации?
-// ❗ В ответе укажите значения через пробел вот так: XXX YYY ZZZ
+// Что должно быть вместо XXX и YYY, чтобы сделать модальное окно доступным?
+// ❗ В ответе укажите значения через пробел, вот так: XXX YYY
