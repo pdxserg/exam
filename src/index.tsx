9f6812b217fc4e99209199a6314bfae5d0b11be4
Sergey Starchenko <pdxserg@gmail.com>
2024-03-18 21:26:44 -0700

    week 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,26 @@
-import ReactDOM from 'react-dom/client';
 import styled from "styled-components";
+import ReactDOM from 'react-dom/client';
 
+const StyledBenefit = styled.button`
+    font-weight: 700;
+    background-color: #5379ed;
+    color: #fff;
+    border-radius: 4px;
+    width: 250px;
+    height: 40px;
+    border: none;
 
-const BirthImg = styled.img`
-    object-fit: contain;
-    max-width: 1200px
+    @media (XXX: 576px) YYY (ZZZ: 768px) {
+        width: 100%;
+    }
 `
-
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<div className="App">
-		<BirthImg
-			src="https://brandlogos.net/wp-content/uploads/2020/09/react-logo-512x512.png"
-			srcSet="https://cdn.iconscout.com/icon/free/png-256/free-node-js-1-1174935.png 85w, https://brandlogos.net/wp-content/uploads/2020/09/react-logo-512x512.png 512w, https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png 1200w"
-			sizes="(max-width: 600px) 85px, (max-width: 800px) 512px, 1200px"
-			alt="Birth" />
+		<StyledBenefit>delphi</StyledBenefit>
 	</div>
 );
 
-
-
-// Посмотрите внимательно на код. Изображение с каким логотипом (Node.js, React или Angular) будет отображаться на экране шириной 768px?
-// ❗ Перед проверкой не забывате очищать cache (при открытой консоли разработчика, правой клавишей мыши по кнопке перезагрузка страницы -> Очистка кэша и жесткая перезагрузка)
+// Что должно быть вместо XXX, YYY и ZZZ, чтобы ширина элемента стала 100% на разрешении в промежутке от 576px до 768px?
+// ❗ В ответе укажите значения через пробел, вот так: XXX YYY ZZZ
