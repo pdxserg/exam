1200d1f41ffd434dfc06d1883e36f52f4f2d5e23
Sergey Starchenko <pdxserg@gmail.com>
2024-03-18 21:24:50 -0700

    week 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,25 @@
 import ReactDOM from 'react-dom/client';
 import styled from "styled-components";
 
-export const StyledAdvantage = styled.div`
-  background: url("https://cdn.iconscout.com/icon/free/png-256/free-css3-8-1175200.png") center/cover no-repeat;
 
-  @media (XXX: YYY), (min-width: 1200px) {
-    width: 500px;
-    height: 500px;
-  }
-  @media  (XXX: ZZZ), (max-width: 768px) {
-    width: 250px;
-    height: 250px;
-  }
-`;
+const BirthImg = styled.img`
+    object-fit: contain;
+    max-width: 1200px
+`
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<div className="App">
-		<StyledAdvantage />
+		<BirthImg
+			src="https://brandlogos.net/wp-content/uploads/2020/09/react-logo-512x512.png"
+			srcSet="https://cdn.iconscout.com/icon/free/png-256/free-node-js-1-1174935.png 85w, https://brandlogos.net/wp-content/uploads/2020/09/react-logo-512x512.png 512w, https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png 1200w"
+			sizes="(max-width: 600px) 85px, (max-width: 800px) 512px, 1200px"
+			alt="Birth" />
 	</div>
 );
 
 
-// Какие значения должны быть вместо XXX, YYY и ZZZ, чтобы для элемента большое изображение было на десктопе или ландшафтной ориентации, а маленькое - на мобилках или портретой ориентации?
-// ❗ В ответе укажите значения через пробел вот так: XXX YYY ZZZ
+
+// Посмотрите внимательно на код. Изображение с каким логотипом (Node.js, React или Angular) будет отображаться на экране шириной 768px?
+// ❗ Перед проверкой не забывате очищать cache (при открытой консоли разработчика, правой клавишей мыши по кнопке перезагрузка страницы -> Очистка кэша и жесткая перезагрузка)
