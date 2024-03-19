edcebe25181642e6411f33b66b0d0fbb23f5ad74
Sergey Starchenko <pdxserg@gmail.com>
2024-03-18 21:21:18 -0700

    week 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,26 @@
-import styled from "styled-components";
 import ReactDOM from 'react-dom/client';
+import styled from "styled-components";
 
+export const StyledAdvantage = styled.div`
+  background: url("https://cdn.iconscout.com/icon/free/png-256/free-css3-8-1175200.png") center/cover no-repeat;
 
-const StyledCousinInput = styled.input`
-    width: 350px;
-    height: 35px;
-    background-color: transparent;
-    border: 1px solid #626673;
-    border-radius: 5px;
-    caret-color: #2d2f38;
-    padding: 8px 20px;
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 20px;
-    color: #2d2f38;
-
-    &:: {
-        font-weight: 600;
-        font-size: 16px;
-        line-height: 20px;
-        color: #a3a9ba;
-    }
-`
-
-
-const CousinInput = () => {
-	return (
-		<StyledCousinInput XXX="Type cousin here..."/>
-	);
-};
-
+  @media (XXX: YYY), (min-width: 1200px) {
+    width: 500px;
+    height: 500px;
+  }
+  @media  (XXX: ZZZ), (max-width: 768px) {
+    width: 250px;
+    height: 250px;
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<div className="App">
-		<CousinInput />
+		<StyledAdvantage />
 	</div>
 );
 
 
-// Что должно быть вместо XXX, чтобы стили применились к подсказке в текстовом поле?
+// Какие значения должны быть вместо XXX, YYY и ZZZ, чтобы для элемента большое изображение было на десктопе или ландшафтной ориентации, а маленькое - на мобилках или портретой ориентации?
+// ❗ В ответе укажите значения через пробел вот так: XXX YYY ZZZ
