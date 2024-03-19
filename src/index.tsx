74728d23009fdad0ba1d5676105189935422a08f
Sergey Starchenko <pdxserg@gmail.com>
2024-03-18 21:28:22 -0700

    week 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,24 @@
-import styled from "styled-components";
 import ReactDOM from 'react-dom/client';
+import styled from "styled-components";
 
-const StyledBenefit = styled.button`
-    font-weight: 700;
-    background-color: #5379ed;
-    color: #fff;
-    border-radius: 4px;
-    width: 250px;
-    height: 40px;
-    border: none;
 
-    @media (XXX: 576px) YYY (ZZZ: 768px) {
-        width: 100%;
-    }
+const CarImg = styled.img`
+    height: 400px;
+    width: 400px;
+    object-fit: contain
 `
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<div className="App">
-		<StyledBenefit>delphi</StyledBenefit>
+		<CarImg
+			src="https://cdn.iconscout.com/icon/free/png-256/free-node-js-1-1174935.png?f=webp"
+
+			XXX="https://brandlogos.net/wp-content/uploads/2020/09/react-logo-512x512.png YYY, https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png ZZZ" />
 	</div>
 );
 
-// Что должно быть вместо XXX, YYY и ZZZ, чтобы ширина элемента стала 100% на разрешении в промежутке от 576px до 768px?
-// ❗ В ответе укажите значения через пробел, вот так: XXX YYY ZZZ
+
+// Что должно быть вместо XXX, YYY и ZZZ, чтобы код работал корректно и для экранов с разной плотностью пикселей загружалось правильное изображение?
+//❗ В ответе укажите значения через пробел, вот так: XXX YYY ZZZ
