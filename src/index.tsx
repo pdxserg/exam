ae54fd066e9799be9324e121fc0c7bab4c063a33
Sergey Starchenko <pdxserg@gmail.com>
2024-03-24 21:54:40 -0700

    exam 4

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,55 @@
 import ReactDOM from 'react-dom/client';
 import styled, { keyframes } from "styled-components";
 
-const appointmentAnimation = keyframes`
-    50% {
-        transform: scale(5)
+
+
+const actorAnimation = keyframes`
+    from {
+        transform: translateY(0) translateX(0);
     }
-    100% {
-        transform: scale(3.5)
+    25% {
+        background-color: red;
+        transform: translateY(500px) translateX(150px);
+    }
+    50% {
+        transform: translateY(400px) translateX(250px);
+    }
+    70% {
+        transform: translateY(500px) translateX(420px);
+    }
+    90% {
+        transform: translateY(480px) translateX(480px);
+    }
+    to {
+        transform: translateY(500px) translateX(500px);
     }
 `
 
-export const Appointment = styled.div`
-  width: 24px;
-	height: 24px;
-  transform: scale(3.5);
-	background: #FF5252;
-	clip-path: path("M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z");
-  margin: 50px;
-  animation: ${appointmentAnimation} 2s infinite;
-  animation-delay: 3s;
+const StyledCommunicationBox = styled.div`
+    height: 500px;
+    width: 500px;
+    border: 1px solid;
+    padding: 20px;
+`
+
+const StyledActor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #c16ae1;
+  transform-origin: center;
+  animation: ${actorAnimation} 2s ease-in;
+  animation-fill-mode: forwards;
 `
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<div className="App">
-		<Appointment/>
+		<StyledCommunicationBox>
+			<StyledActor/>
+		</StyledCommunicationBox>
 	</div>
 );
 
-// Что должно быть вместо XXX и YYY, чтобы анимация началась через 3 секунды после перезагрузки страницы?
-//❗ В ответе укажите значения через пробел: XXX YYY
+// Что должно быть вместо XXX и YYY, чтобы после проигрывания анимация остановилась на последнем ключевом кадре?
+//❗ В ответе укажите значения через пробел, вот так: XXX YYY
