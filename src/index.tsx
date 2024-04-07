52c789e346ccb5ec9aba524f35840af9402adbea
SergeyStarchenko <pdxserg@gmail.com>
2024-04-06 17:44:44 -0700

    01-01

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import ReactDOM from 'react-dom'
+
+const Son = (props: any) => {
+	return <div>
+		I am son. My name is {props.name}
+	</div>
+}
+
+
+const Father = (props: any) => {
+	return <div>
+		I am father. My name is {props.name}
+		<Son name={props.sonName} />
+	</div>
+}
+
+const Granny = (props: any) => {
+	return <div>
+		I am granny. My name is {props.name}
+		<Father name={props.fatherName} sonName={props.sonName} />
+	</div>
+}
+
+export const App = () => {
+	return <div>
+		<Granny name={'Бабуля'} name={'Батя'} name={'Сын'}/>
+	</div>
+}
+
+ReactDOM.render(<App/>,
+	document.getElementById('root')
+)
