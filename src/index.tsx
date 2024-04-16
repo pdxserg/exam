5ae04c12bff225793927eeaee32e4187e629dbfa
SergeyStarchenko <pdxserg@gmail.com>
2024-04-16 15:56:54 -0700

    01-02

--- src/index.tsx
+++ src/index.tsx
@@ -2,45 +2,18 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-type UserType = {
-	id: number
-	name: string
-	age: number
-}
-
-function User(props: UserType) {
-	return (
-		<li>User {props.name}: {props.age} y.o.</li>
-	)
-}
-
 function UsersList() {
-	const state = [
-		{id: 1, name: "Bob", age: 34},
-		{id: 2, name: "Alex", age: 25},
-		{id: 3, name: "Ann", age: 30},
-		{id: 4, name: "John", age: 23},
-	]
-	const users = [
-		{id: 1, userName: "Bob", age: 34},
-		{id: 2, userName: "Alex", age: 25},
-		{id: 3, userName: "Ann", age: 30},
-		{id: 4, userName: "John", age: 23},
-	]
+	const results = useState<UsersList>(["Bob", "Alex", "Ann"])
+	const users = results[0]
+	const setUsers = results[1]
 
-	const [usersList, setUsersList] = useState<Array<UserType>>([])
 	return (
-		<main>
-			<h5>User list:</h5>
-			<p>Тут будет список пользователей</p>
-		</main>
+		<p>Тут будет список пользователей</p>
 	)
 }
 
 ReactDOM.render(
 	<UsersList/>, document.getElementById('root')
 );
-// Что надо написать вместо XXX, чтобы код работал?
-// ❗ Если мы отмапим массив, то должны увидеть данные пользователей
-// ❗ Ответ дать минимально возможным объёмом кода
 
+// Какой тип правильнее указать вместо "any" при типизации стэйта?
