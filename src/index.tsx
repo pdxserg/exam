a0b4db519e9eb01d3264cfb6b8bea40bcd9a1319
SergeyStarchenko <pdxserg@gmail.com>
2024-04-22 16:58:13 -0700

    01-02

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,37 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function UsersList() {
-	const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
-	const getUser = (user: string) => <li>{user}</li>
-	const usersList = users.map(getUser)
+type UserType = {
+	id: number
+	name: string
+	age: number
+}
 
+function User(props: UserType) {
+	return (
+		<li>User {props.name}: {props.age} y.o.</li>
+	)
+}
+
+function UsersList() {
+	const state = [
+		{id: 1, name: "Bob", age: 34},
+		{id: 2, name: "Alex", age: 25},
+		{id: 3, name: "Ann", age: 30},
+		{id: 4, name: "John", age: 23},
+	]
+	const users = [
+		{id: 1, userName: "Bob", age: 34},
+		{id: 2, userName: "Alex", age: 25},
+		{id: 3, userName: "Ann", age: 30},
+		{id: 4, userName: "John", age: 23},
+	]
+
+	const [usersList, setUsersList] = useState<Array<UserType>>(XXX)
 	return (
 		<main>
-			<h4>User list:</h4>
-			<ul>
-				{usersList}
-			</ul>
+			<h5>User list:</h5>
+			<p>Тут будет список пользователей</p>
 		</main>
 	)
 }
@@ -20,4 +40,7 @@ function UsersList() {
 ReactDOM.render(
 	<UsersList/>, document.getElementById('root')
 );
-// Что вернёт выражение: Array.isArray(usersList)?
+// Что надо написать вместо XXX, чтобы код работал?
+// ❗ Если мы отмапим массив, то должны увидеть данные пользователей
+// ❗ Ответ дать минимально возможным объёмом кода
+
