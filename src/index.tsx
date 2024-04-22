6dbc1ea4b705b4b5649e06cad24b7a67fb7238a2
SergeyStarchenko <pdxserg@gmail.com>
2024-04-22 16:48:27 -0700

    01-02

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,22 @@
-const user = { }
-const student = { }
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-user !==  student
-/*Каков результат выполнения этого выражения?*/
+function UsersList() {
+	const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+	const getUser = (user: string) => <li>{user}</li>
+
+	return (
+		<main>
+			<h4>User list:</h4>
+			<ul>
+				{users.map(getUser)}
+			</ul>
+		</main>
+	)
+}
+
+ReactDOM.render(
+	<UsersList/>, document.getElementById('root')
+);
+// Что вернёт выражение: typeof getUser?
