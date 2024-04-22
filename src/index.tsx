b3c70ef1c53bc89b09ca218ce1ce78e9d61f0aa9
SergeyStarchenko <pdxserg@gmail.com>
2024-04-22 16:50:29 -0700

    01-02

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,13 @@ import './index.css';
 
 function UsersList() {
 	const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
-	const getUser = (user: string) => <li>{user}</li>
-
 	return (
-		<main>
-			<h4>User list:</h4>
-			<ul>
-				{users.map(getUser)}
-			</ul>
-		</main>
+		<p>Тут будет список пользователей</p>
 	)
 }
 
 ReactDOM.render(
 	<UsersList/>, document.getElementById('root')
 );
-// Что вернёт выражение: typeof getUser?
+// Что вернёт выражение: typeof setUsers?
+
