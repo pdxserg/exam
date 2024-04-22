c71aa90ede49b66a253e0a305196fe0beab46836
SergeyStarchenko <pdxserg@gmail.com>
2024-04-22 16:53:40 -0700

    01-02

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function UsersList() {
-	const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+	const results = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+	const users = results[0]
+	const setUsers = results[1]
 
 	return (
-		<main>
-			<h4>User list:</h4>
-			<ul>
-				{ users.ХХХ(user => <li>{user}</li> )}
-			</ul>
-		</main>
+		<p>Тут будет список пользователей</p>
 	)
 }
 
 ReactDOM.render(
 	<UsersList/>, document.getElementById('root')
 );
-// Что надо вставить вместо ХХХ, чтобы код заработал?
+
+// Чему равно results.length?
