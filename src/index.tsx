451f7a1f3b31c7fa55cbc72034a29cb045a4fa29
SergeyStarchenko <pdxserg@gmail.com>
2024-04-22 17:54:07 -0700

    01-03

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,29 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-type UserType = {
-	id: number
-	name: string
-	age: number
+function Button() {
+	return <button
+		// onClick={xxx}
+	>Click</button>
 }
 
-function User(props: UserType) {
-	return (
-		<li>Student {props.name}: {props.age} y.o.</li>
-	)
-}
-
-function UsersList() {
-	const data: Array<UserType> = [
-		{id: 1, name: "Bob", age: 34},
-		{id: 2, name: "Alex", age: 29},
-		{id: 3, name: "Ann", age: 25},
-		{id: 4, name: "John", age: 36},
-	]
-	const [users, setUsers] = useState<Array<UserType>>(data)
-	return (
-		<main>
-			<h4>User list:</h4>
-			<ul>
-				{/*{ users.map(u => <User key={u.id} {...xxx} />) }*/}
-			</ul>
-		</main>
-	)
-}
 
 ReactDOM.render(
-	<UsersList/>, document.getElementById('root')
+	<Button/>, document.getElementById('root')
 );
-// Что надо написать вместо xxx, чтобы код работал?
+
+
+
+
+
+// Что надо написать вместо ххх,
+// что бы на странице появился пустой alert при клике по кнопке?
+
+
+
+
+
+
+
+
