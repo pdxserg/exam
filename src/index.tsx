f8864ddaf4e2015e9e9d9743d43ec08f0527d3f2
SergeyStarchenko <pdxserg@gmail.com>
2024-04-22 17:56:44 -0700

    01-03

--- src/index.tsx
+++ src/index.tsx
@@ -8,17 +8,9 @@ type UserType = {
 	age: number
 }
 
-type UserPropsType = UserType & {
-	deleteUser: (id: number) => void
-}
-
-function User(props: UserPropsType) {
-	const deleteUser = () => props.deleteUser(props.id)
+function User(props: UserType) {
 	return (
-		<li>
-			<button onClick={deleteUser}>x</button>
-			User {props.name}: {props.age} y.o.
-		</li>
+		<li>User {props.name}: {props.age} y.o.</li>
 	)
 }
 
@@ -30,19 +22,14 @@ function UsersList() {
 		{id: 4, name: "John", age: 30},
 	]
 	const [users, setUsers] = useState<Array<UserType>>(data)
-	const deleteUser = (userID: number) => {
-		const filteredUsers = users.filter(u => u.id !== userID)
-		// setUsers(xxx)
-	}
+	// Необходимо отрендерить список ользователей старше 25 лет:
+	const getOlderThen25Users = (u: UserType) => u.age > 25
+	const olderThen25Users = users.filter(getOlderThen25Users)
 	return (
 		<main>
 			<h4>User list:</h4>
 			<ul>
-				{users.map(u => <User
-					key={u.id}
-					{...u}
-					deleteUser={deleteUser}
-				/>)}
+				{ olderThen25Users.map(u => <User key={u.id} {...u}/>)}
 			</ul>
 		</main>
 	)
@@ -51,4 +38,4 @@ function UsersList() {
 ReactDOM.render(
 	<UsersList/>, document.getElementById('root')
 );
-// Что надо написать вместо xxx, чтобы код работал?
+// Что вернёт выражение: Array.isArray(olderThen25Users)
