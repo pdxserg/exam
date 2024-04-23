07d4719024b0d6db399f9adeae30f30bfd1121cf
SergeyStarchenko <pdxserg@gmail.com>
2024-04-22 17:48:35 -0700

    01-03

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,38 @@
-const user = {}
-const attention = user
-const student = {}
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-
-if(XXX === user) {
-	console.log ("Yo!!!")
+type UserType = {
+	id: number
+	name: string
+	age: number
 }
 
-/*Какую переменную надо указать вместо XXX, кроме user, что бы в консоль вывелась строка "Yo!!!?*/
+function User(props: UserType) {
+	return (
+		<li>Student {props.name}: {props.age} y.o.</li>
+	)
+}
+
+function UsersList() {
+	const data: Array<UserType> = [
+		{id: 1, name: "Bob", age: 34},
+		{id: 2, name: "Alex", age: 29},
+		{id: 3, name: "Ann", age: 25},
+		{id: 4, name: "John", age: 36},
+	]
+	const [users, setUsers] = useState<Array<UserType>>(data)
+	return (
+		<main>
+			<h4>User list:</h4>
+			<ul>
+				{/*{ users.map(u => <User key={u.id} {...xxx} />) }*/}
+			</ul>
+		</main>
+	)
+}
+
+ReactDOM.render(
+	<UsersList/>, document.getElementById('root')
+);
+// Что надо написать вместо xxx, чтобы код работал?
