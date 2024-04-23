28707dffe13ed7afd65a8c773672b44bfe193f92
SergeyStarchenko <pdxserg@gmail.com>
2024-04-22 17:55:30 -0700

    01-03

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Button() {
-	return <button
-		// onClick={xxx}
-	>Click</button>
+type UserType = {
+	id: number
+	name: string
+	age: number
 }
 
+type UserPropsType = UserType & {
+	deleteUser: (id: number) => void
+}
+
+function User(props: UserPropsType) {
+	const deleteUser = () => props.deleteUser(props.id)
+	return (
+		<li>
+			<button onClick={deleteUser}>x</button>
+			User {props.name}: {props.age} y.o.
+		</li>
+	)
+}
+
+function UsersList() {
+	const data: Array<UserType> = [
+		{id: 1, name: "Bob", age: 25},
+		{id: 2, name: "Alex", age: 28},
+		{id: 3, name: "Ann", age: 23},
+		{id: 4, name: "John", age: 30},
+	]
+	const [users, setUsers] = useState<Array<UserType>>(data)
+	const deleteUser = (userID: number) => {
+		const filteredUsers = users.filter(u => u.id !== userID)
+		// setUsers(xxx)
+	}
+	return (
+		<main>
+			<h4>User list:</h4>
+			<ul>
+				{users.map(u => <User
+					key={u.id}
+					{...u}
+					deleteUser={deleteUser}
+				/>)}
+			</ul>
+		</main>
+	)
+}
 
 ReactDOM.render(
-	<Button/>, document.getElementById('root')
+	<UsersList/>, document.getElementById('root')
 );
-
-
-
-
-
-// Что надо написать вместо ххх,
-// что бы на странице появился пустой alert при клике по кнопке?
-
-
-
-
-
-
-
-
+// Что надо написать вместо xxx, чтобы код работал?
