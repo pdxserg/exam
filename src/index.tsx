2129f67109118e3a72bf8f34a955a6551e5fdcd1
SergeyStarchenko <pdxserg@gmail.com>
2024-04-22 17:38:49 -0700

    01-03

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,42 @@
-const user = { }
-const student = { }
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-user !==  student
-/*Каков результат выполнения этого выражения?*/
+type UserType = {
+	id: number
+	name: string
+	age: number
+}
+
+function User(props: UserType) {
+	return (
+		<li>User {props.name}: {props.age} y.o.</li>
+	)
+}
+
+function UsersList() {
+	const data: Array<UserType> = [
+		{id: 1, name: "Bob", age: 25},
+		{id: 2, name: "Alex", age: 28},
+		{id: 3, name: "Ann", age: 23},
+		{id: 4, name: "John", age: 30},
+	]
+	const [users, setUsers] = useState<Array<UserType>>(data)
+	// Необходимо отрендерить список пользователей старше 25 лет:
+	// const getOlderThen25Users = (u: xxx) => u.age > 25
+	// const olderThen25Users = users.filter(getOlderThen25Users)
+	return (
+		<main>
+			<h4>User list:</h4>
+			<ul>
+				{/*{ olderThen25Users.map(u => <User key={u.id} {...u}/>)}*/}
+			</ul>
+		</main>
+	)
+}
+
+ReactDOM.render(
+	<UsersList/>, document.getElementById('root')
+);
+
+// Что надо написать вместо xxx, чтобы код работал?
