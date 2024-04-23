003e97d6eef5f955b260237a315b26b3a97e120a
SergeyStarchenko <pdxserg@gmail.com>
2024-04-22 17:45:25 -0700

    01-03

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, MouseEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-type UserType = {
-	id: number
-	name: string
-	age: number
-}
-
-function User(props: UserType) {
+function Button() {
+	const [tagName, setTagName] = useState<string>()
+	const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+		// setTagName(e.xxx.tagName)
+	}
 	return (
-		<li>User {props.name}: {props.age} y.o.</li>
-	)
-}
-
-function UsersList() {
-	const data: Array<UserType> = [
-		{id: 1, name: "Bob", age: 25},
-		{id: 2, name: "Alex", age: 28},
-		{id: 3, name: "Ann", age: 23},
-		{id: 4, name: "John", age: 30},
-	]
-	const [users, setUsers] = useState<Array<UserType>>(data)
-	// Пользователи старше 25 лет:
-	// const olderThen25Users = users.xxx(u => u.age > 25)
-	return (
-		<main>
-			<h4>User list:</h4>
-			<ul>
-				{/*{ olderThen25Users.map(u => <User key={u.id} {...u}/>)}*/}
-			</ul>
-		</main>
+		<>
+			<p>{tagName}</p>
+			<button onClick={onClickHandler} >
+				<span>Click</span>
+			</button>
+		</>
 	)
 }
 
 ReactDOM.render(
-	<UsersList/>, document.getElementById('root')
+	<Button/>, document.getElementById('root')
 );
-// Что надо написать вместо xxx, чтобы код работал?
+
+// Что надо написать вместо ххх, что бы на странице появился текст BUTTON?
