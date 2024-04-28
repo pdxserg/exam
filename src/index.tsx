19ba43e562791da649ad360a5df047025e600cee
SergeyStarchenko <pdxserg@gmail.com>
2024-04-28 13:10:30 -0700

    01-04 9

--- src/index.tsx
+++ src/index.tsx
@@ -2,46 +2,20 @@ import React, {useState, MouseEvent, ChangeEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Notes() {
-	const [newNote, setNewNote] = useState<string>("")
-	const [notes, setNotes] = useState<Array<string>>([])
-	const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
-		setNewNote(e.currentTarget.value)
-	const addNote = () => {
-		setNotes([newNote, ...notes])
-		setNewNote("")
-	}
+function User() {
+	const [userName, setUserName] = useState<string>("")
 	return (
 		<div>
-            <textarea
-	            value={newNote}
-	            onChange={onChangeHandler}
-	            onBlur={addNote}
-            />
-			<div>
-				<button
-					// onClick={xxx}
-				>Clear notes list</button>
-			</div>
-			<h4>Notes:</h4>
-			<div>
-				{notes.map(n => <p>{n}</p>)}
-			</div>
+			<p>{userName}</p>
+			<input
+				// xxx
+				onChange={(e) => setUserName(e.currentTarget.value)}
+			/>
 		</div>
 	)
 }
 
 ReactDOM.render(
-	<Notes/>, document.getElementById('root')
+	<User/>, document.getElementById('root')
 );
-// Что надо написать вместо ххх,
-// чтобы при клике список заметок очищался?
-
-
-
-
-
-
-
-
-
+// Что надо написать вместо ххх, чтобы инпут был контролируемым?
