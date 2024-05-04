c26ea2dcf2223f0a3f2af54b6caed69f7732872a
SergeyStarchenko <pdxserg@gmail.com>
2024-05-04 11:39:45 -0700

    01-05 12

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,17 @@
-import React, {useState, MouseEvent, ChangeEvent} from 'react';
+import React, {MouseEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Notes() {
-	const [newNote, setNewNote] = useState<string>("")
-	const [notes, setNotes] = useState<Array<string>>([])
-	const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
-		setNewNote(e.currentTarget.value)
-	const addNote = () => {
-		setNotes([newNote, ...notes])
-		setNewNote("")
+function Button() {
+	const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+		// console.log((typeof e) === ххх)
 	}
-	return (
-		<div>
-            <textarea
-	            value={newNote}
-	            onChange={onChangeHandler}
-	            onBlur={addNote}
-            />
-			<div>
-				<button
-					// onClick={xxx}
-				>Clear notes list</button>
-			</div>
-			<h4>Notes:</h4>
-			<div>
-				{notes.map(n => <p>{n}</p>)}
-			</div>
-		</div>
-	)
+	return <button onClick={onClickHandler} >Click</button>
 }
 
+
 ReactDOM.render(
-	<Notes/>, document.getElementById('root')
+	<Button/>, document.getElementById('root')
 );
-// Что надо написать вместо ххх,
-// чтобы при клике список заметок очищался?
 
-
-
-
-
-
-
-
-
+// Что надо написать вместо ххх, чтобы в консоль вывело true?
