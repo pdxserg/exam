726b6daa001586cf10ac4eaa9e2304a6ea29445f
SergeyStarchenko <pdxserg@gmail.com>
2024-05-03 22:54:30 -0700

    2 try 01-04 7

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,46 @@ import React, {useState, MouseEvent, ChangeEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function User() {
-	const [userName, setUserName] = useState<string>("")
-	// const onChangeHandler = (e: xxx )=> setUserName(e.currentTarget.value)
+function Notes() {
+	const [newNote, setNewNote] = useState<string>("")
+	const [notes, setNotes] = useState<Array<string>>([])
+	const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
+		setNewNote(e.currentTarget.value)
+	const addNote = () => {
+		setNotes([newNote, ...notes])
+		setNewNote("")
+	}
 	return (
 		<div>
-			<p>{userName}</p>
-			<input
-				// onChange={onChangeHandler}
-			/>
+            <textarea
+	            value={newNote}
+	            onChange={onChangeHandler}
+	            onBlur={addNote}
+            />
+			<div>
+				<button
+					// onClick={xxx}
+				>Clear notes list</button>
+			</div>
+			<h4>Notes:</h4>
+			<div>
+				{notes.map(n => <p>{n}</p>)}
+			</div>
 		</div>
 	)
 }
 
 ReactDOM.render(
-	<User/>, document.getElementById('root')
+	<Notes/>, document.getElementById('root')
 );
-// Что надо написать вместо ххх, чтобы правильно типизировать
-// параметр функции?
+// Что надо написать вместо ххх,
+// чтобы при клике список заметок очищался?
+
+
+
+
+
+
+
+
+
