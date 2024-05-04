4b92533ca0979f2a9aa67007d420f4ad1c8f8462
SergeyStarchenko <pdxserg@gmail.com>
2024-05-04 11:32:17 -0700

    01-05 11

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,47 @@
-import React, {useState} from 'react';
+import React, {useState, MouseEvent, ChangeEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-type UserType = {
-	id: number
-	name: string
-	age: number
-}
-
-function User(props: UserType) {
+function Notes() {
+	const [newNote, setNewNote] = useState<string>("")
+	const [notes, setNotes] = useState<Array<string>>([])
+	const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
+		setNewNote(e.currentTarget.value)
+	const addNote = () => {
+		setNotes([newNote, ...notes])
+		setNewNote("")
+	}
 	return (
-		<li>User {props.name}: {props.age} y.o.</li>
-	)
-}
-
-function UsersList() {
-	const data: Array<UserType> = [
-		{id: 1, name: "Bob", age: 25},
-		{id: 2, name: "Alex", age: 28},
-		{id: 3, name: "Ann", age: 23},
-		{id: 4, name: "John", age: 30},
-	]
-	const [users, setUsers] = useState<Array<UserType>>(data)
-	// Пользователи старше 25 лет:
-	// const olderThen25Users = users.xxx(u => u.age > 25)
-	return (
-		<main>
-			<h4>User list:</h4>
-			<ul>
-				{/*{ olderThen25Users.map(u => <User key={u.id} {...u}/>)}*/}
-			</ul>
-		</main>
+		<div>
+            <textarea
+	            value={newNote}
+	            onChange={onChangeHandler}
+	            onBlur={addNote}
+            />
+			<div>
+				<button
+					// onClick={xxx}
+				>Clear notes list</button>
+			</div>
+			<h4>Notes:</h4>
+			<div>
+				{notes.map(n => <p>{n}</p>)}
+			</div>
+		</div>
 	)
 }
 
 ReactDOM.render(
-	<UsersList/>, document.getElementById('root')
+	<Notes/>, document.getElementById('root')
 );
-// Что надо написать вместо xxx, чтобы код работал?
+// Что надо написать вместо ххх,
+// чтобы при клике список заметок очищался?
+
+
+
+
+
+
+
+
+
