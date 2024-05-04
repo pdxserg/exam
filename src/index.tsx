5fda0d55e31480046855dd6441c3b65e4c47bb65
SergeyStarchenko <pdxserg@gmail.com>
2024-05-04 10:49:18 -0700

    01-05 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,46 @@
-import React, {useState, MouseEvent, ChangeEvent} from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function User() {
-	const [userName, setUserName] = useState<string>("")
-	const [text, setText] = useState<string>("")
-	const onChangeHandler = (e: ChangeEvent<HTMLInputElement> )=> setUserName(e.currentTarget.value)
+type UserType = {
+	id: number
+	name: string
+	age: number
+}
+
+function User(props: UserType) {
 	return (
-		<div>
-			<input
-				value={userName}
-				onChange={onChangeHandler}
-				onBlur={()=> {
-					// xxx
-					setText(userName)
-				}}
-			/>
-			<p>{text}</p>
-		</div>
+		<li>User {props.name}: {props.age} y.o.</li>
+	)
+}
+
+function UsersList() {
+	const state = [
+		{id: 1, name: "Bob", age: 34},
+		{id: 2, name: "Alex", age: 25},
+		{id: 3, name: "Ann", age: 30},
+		{id: 4, name: "John", age: 23},
+	]
+	const users = [
+		{id: 1, userName: "Bob", age: 34},
+		{id: 2, userName: "Alex", age: 25},
+		{id: 3, userName: "Ann", age: 30},
+		{id: 4, userName: "John", age: 23},
+	]
+
+	const [usersList, setUsersList] = useState<Array<UserType>>(XXX)
+	return (
+		<main>
+			<h5>User list:</h5>
+			<p>Тут будет список пользователей</p>
+		</main>
 	)
 }
 
 ReactDOM.render(
-	<User/>, document.getElementById('root')
+	<UsersList/>, document.getElementById('root')
 );
-// Что надо написать вместо ххх,
-// чтобы после вывода текста в параграф содержимое формы ввода очищалось?
+// Что надо написать вместо XXX, чтобы код работал?
+// ❗ Если мы отмапим массив, то должны увидеть данные пользователей
+// ❗ Ответ дать минимально возможным объёмом кода
+
