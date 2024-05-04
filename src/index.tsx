764d2ece6310b7639d781bcabfb01398bce3c170
SergeyStarchenko <pdxserg@gmail.com>
2024-05-04 11:10:07 -0700

    01-05 5

--- src/index.tsx
+++ src/index.tsx
@@ -1,53 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, MouseEvent, ChangeEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-type UserType = {
-	id: number
-	name: string
-	age: number
-}
-
-type UserPropsType = UserType & {
-	deleteUser: (id: number) => void
-}
-
-function User(props: UserPropsType) {
+function User() {
+	const [userName, setUserName] = useState<string>("")
 	return (
-		<li>
-			{/*<button onClick={() => props.deleteUser(xxx)}>x</button>*/}
-			User {props.name}: {props.age} y.o.
-		</li>
-	)
-}
-
-function UsersList() {
-	const data: Array<UserType> = [
-		{id: 1, name: "Bob", age: 25},
-		{id: 2, name: "Alex", age: 28},
-		{id: 3, name: "Ann", age: 23},
-		{id: 4, name: "John", age: 30},
-	]
-	const [users, setUsers] = useState<Array<UserType>>(data)
-	const deleteUser = (userID: number) => {
-		const filteredUsers = users.filter(u => u.id !== userID)
-		setUsers(filteredUsers)
-	}
-	return (
-		<main>
-			<h4>User list:</h4>
-			<ul>
-				{users.map(u => <User
-					key={u.id}
-					{...u}
-					deleteUser={deleteUser}
-				/>)}
-			</ul>
-		</main>
+		<div>
+			<p>{userName}</p>
+			<input
+				// xxx
+				onChange={(e) => setUserName(e.currentTarget.value)}
+			/>
+		</div>
 	)
 }
 
 ReactDOM.render(
-	<UsersList/>, document.getElementById('root')
+	<User/>, document.getElementById('root')
 );
-// Что надо написать вместо xxx, чтобы код работал?
+// Что надо написать вместо ххх, чтобы инпут был контролируемым?
