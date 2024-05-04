53427c13e63df5ff4d2561bf8b7875b488874071
SergeyStarchenko <pdxserg@gmail.com>
2024-05-03 23:00:28 -0700

    2 try 01-04 9

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,53 @@
-import React, {useState, MouseEvent, ChangeEvent} from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Notes() {
-	const [newNote, setNewNote] = useState<string>("")
-	const [notes, setNotes] = useState<Array<string>>([])
-	const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
-		setNewNote(e.currentTarget.value)
-	const addNote = () => {
-		setNotes([newNote, ...notes])
-		setNewNote("")
+type UserType = {
+	id: number
+	name: string
+	age: number
+}
+
+type UserPropsType = UserType & {
+	deleteUser: (id: number) => void
+}
+
+function User(props: UserPropsType) {
+	return (
+		<li>
+			{/*<button onClick={() => props.deleteUser(xxx)}>x</button>*/}
+			User {props.name}: {props.age} y.o.
+		</li>
+	)
+}
+
+function UsersList() {
+	const data: Array<UserType> = [
+		{id: 1, name: "Bob", age: 25},
+		{id: 2, name: "Alex", age: 28},
+		{id: 3, name: "Ann", age: 23},
+		{id: 4, name: "John", age: 30},
+	]
+	const [users, setUsers] = useState<Array<UserType>>(data)
+	const deleteUser = (userID: number) => {
+		const filteredUsers = users.filter(u => u.id !== userID)
+		setUsers(filteredUsers)
 	}
 	return (
-		<div>
-            <textarea
-	            value={newNote}
-	            onChange={onChangeHandler}
-	            onBlur={addNote}
-            />
-			<div>
-				<button
-					// onClick={xxx}
-				>Clear notes list</button>
-			</div>
-			<h4>Notes:</h4>
-			<div>
-				{notes.map(n => <p>{n}</p>)}
-			</div>
-		</div>
+		<main>
+			<h4>User list:</h4>
+			<ul>
+				{users.map(u => <User
+					key={u.id}
+					{...u}
+					deleteUser={deleteUser}
+				/>)}
+			</ul>
+		</main>
 	)
 }
 
 ReactDOM.render(
-	<Notes/>, document.getElementById('root')
+	<UsersList/>, document.getElementById('root')
 );
-// Что надо написать вместо ххх,
-// чтобы при клике список заметок очищался?
-
-
-
-
-
-
-
-
-
+// Что надо написать вместо xxx, чтобы код работал?
