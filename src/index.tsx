d1393028746d4b4396035e9af816d443481eb2d6
SergeyStarchenko <pdxserg@gmail.com>
2024-05-03 22:48:29 -0700

    2 try 01-04 4

--- src/index.tsx
+++ src/index.tsx
@@ -1,55 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, MouseEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-type UserType = {
-	id: number
-	name: string
-	age: number
-}
-
-type UserPropsType = UserType & {
-	deleteUser: (id: number) => void
-}
-
-function User(props: UserPropsType) {
-	const deleteUser = () => props.deleteUser(props.id)
-	return (
-		<li>
-			{/*<button onClick={xxx}>Delete</button>*/}
-			User {props.name}: {props.age} y.o.
-		</li>
-	)
-}
-
-function UsersList() {
-	const data: Array<UserType> = [
-		{id: 1, name: "Bob", age: 25},
-		{id: 2, name: "Alex", age: 28},
-		{id: 3, name: "Ann", age: 23},
-		{id: 4, name: "John", age: 30},
-	]
-	const [users, setUsers] = useState<Array<UserType>>(data)
-	const deleteUser = (userID: number) => {
-		const updatedUsers = users.filter(u => u.id !== userID)
-		setUsers(updatedUsers)
+function AuthForm() {
+	const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+		// xxx
+		alert()
 	}
 	return (
-		<main>
-			<h4>User list:</h4>
-			<ul>
-				{users.map(u => <User
-					key={u.id}
-					{...u}
-					deleteUser={deleteUser}
-				/>)}
-			</ul>
-		</main>
+		<form>
+			<div>
+				<label style={{padding: "10px 0"}}>
+					Name:
+					<input type={"email"} name={"email"}/>
+				</label>
+			</div>
+			<div>
+				<label style={{padding: "10px 0"}}>
+					Password:
+					<input type={"password"} name={"password"}/>
+				</label>
+			</div>
+			<button
+				onClick={onClickHandler}
+				type={"submit"}>
+				Log in
+			</button>
+		</form>
 	)
 }
 
 ReactDOM.render(
-	<UsersList/>, document.getElementById('root')
+	<AuthForm/>, document.getElementById('root')
 );
-
-// Что надо написать вместо xxx, чтобы код работал?
+// Что надо написать вместо ххх, чтобы данные из формы
+// не отправлялись на сервер и страница не перезагружалась
+// при клике по кнопке?
