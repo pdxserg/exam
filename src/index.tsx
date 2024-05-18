6db3351c8f82d05d9415653619bfd4369f661641
SergeyStarchenko <pdxserg@gmail.com>
2024-05-17 21:56:28 -0700

    02-02 9

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,9 @@
-type StudentType = {
-	id: number
-	name: string
-}
+const members = ["Elizaveta", "Vanya", "Sergey"]
+const advantage = [... members]
+const appearance = advantage
 
-type FriendsType = {
-	[key: string]: Array<string>
-}
+const result = appearance !== XXX
+	? "Yo!!!"
+	: "Hey!!!"
 
-export const students: Array<StudentType> = [
-	{id: 1, name: "Bob"},
-	{id: 2, name: "Alex"},
-	{id: 3, name: "Ann"},
-	{id: 4, name: "Charley"},
-]
-
-export const friends: FriendsType = {
-	1: ["Oliver", "Jack", "Oscar",],
-	2: ["Jack", "Lewis", "Thomas",],
-	3: ["William", "Michael", "Lewis",],
-	4: ["Oscar", "James", "William",],
-}
-
-//Дан список студентов и структура,
-//которая содержит список друзей каждого из студентов.
-//Id студента является ключом к массиву его друзей.
-//Какое значение лежит тут: friends[students[1].id][2]?
+/*Имя какой переменной надо указать вместо XXX, что бы переменная result получила значение "Yo!!!"? Если такой переменной нет, в качестве ответа укажите null.*/
