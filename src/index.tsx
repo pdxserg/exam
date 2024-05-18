2bae2aa3aaf48f7a697fb8b518fcbb4dd3e75358
SergeyStarchenko <pdxserg@gmail.com>
2024-05-17 21:48:32 -0700

    02-02 5

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,31 @@
-const user = {
-	name: "Mia",
-	age: 90,
-	friends: ["Maxim", "Victor", "Sergey"]
+type StudentType = {
+	id: number
+	name: string
+	age: number
 }
 
-const newFriend = "Margo"
+type FriendsType = {
+	[key: string]: Array<string>
+}
 
-const friends = [XXX, newFriend]
 
-const updatedUser = {...user, friends: friends}
+export const students: Array<StudentType> = [
+	{id: 1, name: "Bob", age: 34},
+	{id: 2, name: "Alex", age: 23},
+	{id: 3, name: "Ann", age: 25},
+	{id: 4, name: "Charley", age: 20},
+]
 
-/*Выполнено преобразование объекта: добавлен новый друг в список.
-Что надо указать вместо XXX, что бы выполнить это действие иммутабельно?*/
+export const friends: FriendsType = {
+	1: ["Jack","Oliver", "Oscar",],
+	2: ["Jack", "Lewis", "Thomas",],
+	3: ["William",  "Lewis","Michael"],
+	4: ["Oscar", "Thomas", "William",],
+}
+
+//Дан массив студентов и структура,
+//которая содержит список друзей каждого из студентов.
+//Id студента является ключом к массиву его друзей.
+//Имеют ли студенты students[1] и students[3] общих друзей?
+//Если да, напишите в ответе имя общего друга в кавычках, если нет - напишите в ответе false.
+
