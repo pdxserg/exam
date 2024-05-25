7fe4740c7e87cbb1e5d67f3dd280f2910a938666
SergeyStarchenko <pdxserg@gmail.com>
2024-05-25 16:27:10 -0700

    02-03 9

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,12 @@
-import React, {useState} from "react";
+const names = ["Igor", "Nikita", "Natali"]
 
-type UserType = {
-	id: number
-	name: string
-	status: "online" | "offline"
-}
+const nameNext = names
 
-type AddressType = {
-	country: string
-	city: string
-	email: string
-}
+nameNext.length = 0
 
-type AdressesType = {
-	[userID: string]: AddressType
-}
+const result = names[0] ? 91 : 40
 
-const users: Array<UserType> = [
-	{id: 1, name: "Bob", status: "online"},
-	{id: 2, name: "Alex", status: "offline"},
-	{id: 3, name: "Ann", status: "offline"},
-]
+/* Какое значение получит переменная result? */
 
-const addresses: AdressesType = {
-	1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
-	2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
-	3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
 
-}
 
-const updateUserAddress = (userID: number, key: string, newValue: string) => {
-	return {...addresses,
-		[userID]: {...addresses[xxx], [key]: newValue}
-	}
-}
-// Дан список пользователей и структура, хранящая адреса пользователей.
-// Так же дана функция updateUserAddress,
-// которая обновляет указанное в параметрах поле в адресе пользователя.
-// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
-// Что надо написать вместо ххх, чтобы функция работала корректно?
