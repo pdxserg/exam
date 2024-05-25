f19a15297079d92c1d2b949a4a3dee1b78c650ea
SergeyStarchenko <pdxserg@gmail.com>
2024-05-25 16:07:17 -0700

    02-03 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,61 +1,38 @@
 import React, {useState} from "react";
 
-type samuraiType = {
-	id: string
+type UserType = {
+	id: number
 	name: string
 	status: "online" | "offline"
 }
-type CourseNameType = "HTML"|"JS"|"React"|"Redux"|"HomeWorks"
-type CourseType = {
-	name: CourseNameType
-	mentor: string
-	isDone: boolean
-}
-type TechnologiesType = {
-	[userID: string]: Array<CourseType>
+
+type AddressType = {
+	country: string
+	city: string
+	email: string
 }
 
-const samuraiID_1 = "64jf-87kg"
-const samuraiID_2 = "90lg-34ks"
-const samuraiID_3 = "12jm-05fd"
+type AdressesType = {
+	[userID: string]: AddressType
+}
 
-export const samurai: Array<samuraiType> = [
-	{id: samuraiID_1, name: "Bob", status: "online"},
-	{id: samuraiID_2, name: "Alex", status: "offline"},
-	{id: samuraiID_3, name: "Ann", status: "offline"},
+
+const users: Array<UserType> = [
+	{id: 1, name: "Bob", status: "online"},
+	{id: 2, name: "Alex", status: "offline"},
+	{id: 3, name: "Ann", status: "offline"},
 ]
 
-export const technologies: TechnologiesType = {
-	[samuraiID_1]: [
-		{name: "HTML", mentor: "Svetlana", isDone: true},
-		{name: "JS", mentor: "Viktor", isDone: true},
-		{name: "React", mentor: "Dmitrij", isDone: false},
-		{name: "Redux", mentor: "Valera", isDone: false},
-		{name: "HomeWorks", mentor: "Ignat", isDone: true},
-	],
-	[samuraiID_2]: [
-		{name: "HTML", mentor: "Svetlana", isDone: true},
-		{name: "JS", mentor: "Viktor", isDone: true},
-		{name: "React", mentor: "Dmitrij", isDone: true},
-		{name: "Redux", mentor: "Valera", isDone: false},
-		{name: "HomeWorks", mentor: "Ignat", isDone: true},
-	],
-	[samuraiID_3]: [
-		{name: "HTML", mentor: "Svetlana", isDone: true},
-		{name: "JS", mentor: "Viktor", isDone: true},
-		{name: "React", mentor: "Dmitrij", isDone: false},
-		{name: "Redux", mentor: "Valera", isDone: false},
-		{name: "HomeWorks", mentor: "Ignat", isDone: false},
-	],
+export const addresses: AdressesType = {
+	1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
+	2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
+	3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
+
 }
 
-export const updateCourseStatus = (samuraiID: string, name: CourseNameType, isDone: boolean) => {
-	return {...technologies,
-		[samuraiID]: technologies[xxx].map(c => c.name === name ? {...c, isDone} : c)
-	}
+export const changeUserStatus = (userID: number, status: string) => {
+	return users.xxx(u => u.id === userID ? {...u, status} : u)
 }
-// Дан список самураев из инкубатора и структура, хранящая данные о курсах,
-// которые самурай уже прошёл, а так же о тех курсах, которые ему ещё предстоит пройти.
-// Так же дана функция updateCourseStatus,
-// которая позволяет отметить курс как пройденный самураем или снять такую отметку
+// Дан список пользователей и структура, хранящая адреса пользователей.
+// Так же дана функция changeUserStatus, которая меняет статус пользователя.
 // Что надо написать вместо ххх, чтобы функция работала корректно?
