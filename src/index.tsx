e1c3efa49ee00e5e38ce7322cec8ca4acdebbb7a
SergeyStarchenko <pdxserg@gmail.com>
2024-05-31 20:01:23 -0700

    02-0 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,30 @@
-import React, {useState} from "react";
-
 type UserType = {
 	id: number
-	name: string
-	status: "online" | "offline"
-}
-
-type AddressType = {
-	country: string
-	city: string
+	userName: string
 	email: string
+	password: string
 }
 
-type AdressesType = {
-	[userID: string]: AddressType
-}
-
-const users: Array<UserType> = [
-	{id: 1, name: "Bob", status: "online"},
-	{id: 2, name: "Alex", status: "offline"},
-	{id: 3, name: "Ann", status: "offline"},
-]
-
-const addresses: AdressesType = {
-	1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
-	2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
-	3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
-
-}
-
-const updateUserAddress = (userID: number, key: string, newValue: string) => {
-	return {...addresses,
-		[userID]: {...addresses[xxx], [key]: newValue}
+type ChangeUserPasswordTypeAT = {
+	type: "CHANGE-USER-PASSWORD"
+	payload: {
+		XXX
+		YYY
 	}
 }
-// Дан список пользователей и структура, хранящая адреса пользователей.
-// Так же дана функция updateUserAddress,
-// которая обновляет указанное в параметрах поле в адресе пользователя.
-// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
-// Что надо написать вместо ххх, чтобы функция работала корректно?
+
+export const userReducer =
+	(state: UserType[], action: ChangeUserPasswordTypeAT): UserType[] => {
+		switch (action.type) {
+			case "CHANGE-USER-PASSWORD":
+				return state.map(u =>
+					u.id === action.payload.id
+						? {...u, password: action.payload.newPassword}
+						: u)
+			default:
+				return state
+		}
+	}
+
+//Какой код должен быть написан вместо XXX и YYY в типе //ChangeUserPasswordTypeAT, что бы редьюсер работал?
+//В ответе напишите через пробел: XXX  YYY
