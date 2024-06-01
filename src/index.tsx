376cec6d8b5b78760dbf465f32c150c2f48921f8
SergeyStarchenko <pdxserg@gmail.com>
2024-05-31 19:57:45 -0700

    02-0 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,41 @@
-let result = 0
+import React, {useState} from "react";
 
-for(let i = 0; i < 5; i++){
-	if(i % 2){
-		result += i
-	}
+type UserType = {
+	id: number
+	name: string
+	status: "online" | "offline"
 }
 
-const studentName = (result - 4)
-	? "Anastasiya"
-	: "Vanya"
+type AddressType = {
+	country: string
+	city: string
+	email: string
+}
 
-/* Какое значение получит переменная studentName? */
+type AdressesType = {
+	[userID: string]: AddressType
+}
+
+const users: Array<UserType> = [
+	{id: 1, name: "Bob", status: "online"},
+	{id: 2, name: "Alex", status: "offline"},
+	{id: 3, name: "Ann", status: "offline"},
+]
+
+const addresses: AdressesType = {
+	1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
+	2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
+	3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
+
+}
+
+const updateUserAddress = (userID: number, key: string, newValue: string) => {
+	return {...addresses,
+		[userID]: {...addresses[xxx], [key]: newValue}
+	}
+}
+// Дан список пользователей и структура, хранящая адреса пользователей.
+// Так же дана функция updateUserAddress,
+// которая обновляет указанное в параметрах поле в адресе пользователя.
+// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
+// Что надо написать вместо ххх, чтобы функция работала корректно?
