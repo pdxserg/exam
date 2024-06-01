2ebb3b22a9ee47e9717ea14fe7e9776c05969a03
SergeyStarchenko <pdxserg@gmail.com>
2024-05-31 18:59:41 -0700

    02-04 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,28 @@
-const getArray = (a, b, c) => {
-	return [c, b, a]
+
+type ActionType = {
+	type: "SUM"|"SUB"|"MULT"|"DIV"|"EXP"
+	payload: number
 }
 
-const [cabinet, collection, airport]  = getArray(97, 73, 14)
+export const calculator = (state: number, action: ActionType): number => {
+	switch (action.type) {
+		case "SUM":
+			return state + action.payload
+		case "SUB":
+			return state - action.payload
+		case "DIV":
+			return state / action.payload
+		case "MULT":
+			return state * action.payload
+		case "EXP":
+			return state ** action.payload
+		default:
+			return state
+	}
+}
+const result = calculator(10, {XXX, payload: 0})
+if (!(result - 1)){
+	console.log("IT-INCUBATOR")
+}
 
-console.log(cabinet)
-
-/*Какое число будет выведено в консоль?*/
+//Что надо написать вместо XXX чтобы в консоли появилась строка "IT-INCUBATOR"?
