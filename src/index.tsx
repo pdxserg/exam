0a3b708b16f61a5734f3168cbaffe421f3a296df
SergeyStarchenko <pdxserg@gmail.com>
2024-05-31 19:02:46 -0700

    02-04 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,12 @@
-import {type} from "node:os";
+const name = "Eva"
 
-type ActionType = {
-	type: "SUM"|"SUB"|"MULT"|"DIV"|"EXP"
-	payload: number
+const student = {
+	[name]: name
 }
 
-export const calculator = (state: number, action: ActionType): number => {
-	switch (action.type) {
-		case "SUM":
-			return state + action.payload
-		case "SUB":
-			return state - action.payload
-		case "DIV":
-			return state / action.payload
-		case "MULT":
-			return state * action.payload
-		case "EXP":
-			return state ** action.payload
-		default:
-			return state
-	}
-}
-const result = calculator(10, {type, payload: 0})
-if (!(result - 1)){
-	console.log("IT-INCUBATOR")
-}
+const number = student["name"]
+	? 37.76
+	: 43.23
 
+/*Какое значение получит переменная number?*/
 //Что надо написать вместо XXX чтобы в консоли появилась строка "IT-INCUBATOR"?
