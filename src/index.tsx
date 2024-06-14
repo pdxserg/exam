86278991deb3875add3798c219eb760cbb68de1c
SergeyStarchenko <pdxserg@gmail.com>
2024-06-13 20:51:28 -0700

    03-1-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,34 @@
-type UserType = {
-	id: number
-	userName: string
-	email: string
-	password: string
+type Status = 'Stopped' | 'Playing' | 'Paused'
+type StateType = {
+	volume: number // in percents
+	trackUrl: string // 'https://blabla.com/track01.mp3',
+	currentPlayPosition: number // milliseconds,
+	status: Status
 }
-
-type ChangeUserPasswordTypeAT = {
-	type: "CHANGE-USER-PASSWORD"
-	payload: {
-		XXX
-		YYY
+export const playerReducer = (state: StateType, action: any) => {
+	switch (action.type) {
+		case 'TRACK-VOLUME-CHANGED':
+			return {
+				...state,
+				XXX
+			}
+		default:
+			return state
 	}
 }
 
-export const userReducer =
-	(state: UserType[], action: ChangeUserPasswordTypeAT): UserType[] => {
-		switch (action.type) {
-			case "CHANGE-USER-PASSWORD":
-				return state.map(u =>
-					u.id === action.payload.id
-						? {...u, password: action.payload.newPassword}
-						: u)
-			default:
-				return state
-		}
-	}
+const muteTrackAC = () => ({type: 'TRACK-MUTED'})
+const changeVolumeAC = (volumeLevel: number) => ({type: 'TRACK-VOLUME-CHANGED', volumeLevel})
+const changeTrackAC = (url: string) => ({type: 'TRACK-URL-CHANGED', url})
+const changeTrackPlayStatusAC = (status: Status) => ({type: 'TRACK-STATUS-CHANGED', status})
 
-//Какой код должен быть написан вместо XXX и YYY в типе //ChangeUserPasswordTypeAT, что бы редьюсер работал?
-//В ответе напишите через пробел: XXX  YYY
+const state: StateType = {
+	status: 'Stopped',
+	currentPlayPosition: 1213,
+	trackUrl: 'https://blabla.com/track01.mp3',
+	volume: 100
+}
+const newState = playerReducer(state, changeVolumeAC(20))
+console.log(newState.volume === 20)
+
+// Напишите вместо XXX правильную строку кода, чтобы изменить громкость трека и увидеть в консоли true.
