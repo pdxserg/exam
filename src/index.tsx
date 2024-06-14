47a14fb756acbf2dc3a1008fadc6c8648c309154
SergeyStarchenko <pdxserg@gmail.com>
2024-06-13 21:17:35 -0700

    03-1-8

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,26 @@
-export const reducer = (state: any, action: any) => {
+type Status = 'Stopped' | 'Playing' | 'Paused'
+type StateType = {
+	volume: number // in percents
+	trackUrl: string // 'https://blabla.com/track01.mp3',
+	currentPlayPosition: number // milliseconds,
+	status: Status
+}
+
+export const playerReducer = (state: StateType, action: any) => {
 	switch (action.type) {
-		case 'TRACK-LIKED':
+		case XXX:
 			return {
 				...state,
-				[XXX]: {
-					...state[XXX],
-					likesCount: state[XXX].likesCount + 1
-				}
+				trackUrl: action.url
 			}
 		default:
 			return state
 	}
 }
 
-const likeTrackAC = (trackId: number) => ({type: 'TRACK-LIKED', trackId})
+const muteTrackAC = () => ({type: 'TRACK-MUTED'})
+const changeTrackAC = (url: string) => ({type: 'TRACK-URL-CHANGED', url})
+const changeTrackPlayStatusAC = (status: Status) => ({type: 'TRACK-STATUS-CHANGED', status})
 
-
-const state = {
-	12: {id: 12, likesCount: 10},
-	14: {id: 14, likesCount: 2},
-	100: {id: 100, likesCount: 0},
-}
-const newState = reducer(state, likeTrackAC(14))
-
-console.log(newState[14].likesCount === 3)
-
-// Что нужно написать вместо XXX, чтобы в консоли увидеть true?
-// ❗ Захардкодженные значения использовать запрещено
+//Какой тип должен быть вместо XXX?
+ 
