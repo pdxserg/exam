5aa1a182d902736ad39da17b032ac38e01fe8906
SergeyStarchenko <pdxserg@gmail.com>
2024-06-26 21:57:26 -0700

    03-2-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,75 @@
-export const reducer = (state: any, action: any) => {
+import {createStore} from 'redux'
+import ReactDOM from 'react-dom'
+import {Provider, useSelector, useDispatch} from 'react-redux'
+import React from 'react'
+
+const students = {
+	students: [
+		{id: 1, name: 'Bob'},
+		{id: 2, name: 'Alex'},
+		{id: 3, name: 'Donald'},
+		{id: 4, name: 'Ann'},
+	]
+}
+type RemoveStudentAT = {
+	type: "REMOVE-STUDENT"
+	id: number
+}
+const RemoveStudentAC = (id: number): RemoveStudentAT => ({
+	type: "REMOVE-STUDENT",
+	id
+})
+
+const studentsReducer = (state = students, action: RemoveStudentAT) => {
 	switch (action.type) {
-		case 'TRACK-ADDED':
-			return [{id:action.trackId, likesCount: 0},...state]
-		default:
-			return state
+		case "REMOVE-STUDENT":
+			return {
+				...state,
+				students: state.students.filter(s => s.id !== action.id)
+			}
 	}
+	return state
 }
 
-const addTrackAC = (trackId: number) => ({type: 'TRACK-ADDED', trackId})
+const store = createStore(studentsReducer)
+type RootStateType = ReturnType<typeof studentsReducer>
 
 
-const state = [
-	{id: 12, likesCount: 10},
-	{id: 14, likesCount: 2},
-	{id: 100, likesCount: 0}
-]
-const newState = reducer(state, addTrackAC(300))
+const StudentList = () => {
+	const listItemStyles = {
+		width: "100px",
+		borderBottom: "1px solid gray",
+		cursor: "pointer",
+	}
+	const students = useSelector((state: RootStateType) => state.students)
+	const dispatch = useDispatch()
+	const studentsList = students.map(s => {
+		const removeStudent = () => {
+			XXX(YYY( ZZZ))
+		}
+		return (
+			<li key={s.id}
+			    style={listItemStyles}
+			    onClick={removeStudent}>
+				{s.name}
+			</li>)
+	})
+	return (
+		<ol>
+			{studentsList}
+		</ol>
 
-console.log(newState[3].likesCount === 0)
+	)
+}
 
-// Что нужно написать вместо XXX, чтобы трек корректно добавился и в консоли увидеть true?
+
+ReactDOM.render(<div>
+		<Provider store={store}>
+			<StudentList/>
+		</Provider>
+	</div>,
+	document.getElementById('root')
+)
+
+// Что нужно написать вместо XXX, YYY и ZZZ, чтобы при клике по имени студент
+// удалялся из списка? Напишите через пробел.
