c67f3419a6eceb0b92dc1f5fb8ce54ada55ce358
SergeyStarchenko <pdxserg@gmail.com>
2024-06-26 22:08:13 -0700

    03-2-4

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,49 @@
-let number = 92
+import {combineReducers, createStore} from 'redux'
+import ReactDOM from 'react-dom'
+import {Provider, useSelector} from 'react-redux'
+import React from 'react'
 
-if (number > 0) {
-	let number = 10
-	number++
+let initialState = {items:
+		[
+			{id: 1, name: 'Dimych'},
+			{id: 2, name: 'Ignat'}
+		]
+}
+const usersReducer = (state = initialState, action: any) => {
+	return state
 }
 
-const getNumber = (number: any) => {
-	number *= 10
-	return number
+let authInitialState = {login: 'Margo', settings: {theme: 'dark'}}
+const authReducer = (state = authInitialState, action: any) => {
+	return state
 }
 
-const bigValue= getNumber("number") || number
+let rootReducer = combineReducers({
+	users: usersReducer,
+	auth: authReducer
+})
 
-//Какое значение получит переменная bigValue?
+const store = createStore(rootReducer)
+type RootStateType = ReturnType<typeof rootReducer>
+
+const selector = (state: RootStateType) => state.users.items
+
+const Users = () => {
+
+	const users = XXX
+
+	return <ul>
+		{users.map(u => <li key={u.id}>{u.name}</li>)}
+	</ul>
+}
+
+ReactDOM.render(<div>
+		<Provider store={store}>
+			<Users/>
+		</Provider>
+	</div>,
+	document.getElementById('root')
+)
+
+// Что нужно написать вместо XXX, чтобы отрендерить список юзеров?
+// ❗ Ответ дать минимально возможным объёмом кода 
