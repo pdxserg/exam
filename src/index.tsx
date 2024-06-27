3b906f67b6a1179f21106bd873418e94de45b3fe
SergeyStarchenko <pdxserg@gmail.com>
2024-06-26 22:16:22 -0700

    03-2-6

--- src/index.tsx
+++ src/index.tsx
@@ -1,67 +1,8 @@
-import React from 'react'
-import { createStore } from 'redux'
-import { Provider, useSelector, useDispatch } from 'react-redux'
-import ReactDOM from 'react-dom'
 
-type StudentType = {
-	id: number
-	name: string
-	age: number
-}
+const myFunction = (a: number, b = 3) => a + b;
 
-const initState = {
-	students:
-		[
-			{id: 1, name: 'Bob', age: 23},
-			{id: 2, name: 'Alex', age: 22}
-		] as Array<StudentType>
-}
-type AddStudentAT = {
-	type: 'ADD-STUDENT'
-	name: string
-	age: number
-	id: number
-}
+const result = myFunction(346, undefined) % 2
 
-type InitialStateType = typeof initState
+const winner = result ? "Artem" : "Artem"
 
-const studentsReducer = (state: InitialStateType = initState, action: AddStudentAT): InitialStateType => {
-	switch (action.type) {
-		case 'ADD-STUDENT':
-			return {
-				...state,
-				students: [...state.students, {
-					name: action.name,
-					age: action.age,
-					id: action.id
-				}]
-			}
-	}
-	return state
-}
-
-const appStore = createStore(studentsReducer)
-type RootStateType = ReturnType<typeof studentsReducer>
-
-
-const StudentList = () => {
-	const students = useSelector((state: RootStateType) => state.students)
-	return (
-		<ul>
-			{students.map(s => <li key={s.id}>{`${s.name}. ${s.age} years.`}</li>)}
-		</ul>
-	)
-}
-const App = () => {
-	return <StudentList/>
-}
-
-ReactDOM.render(<div>
-		<XXX YYY={ZZZ}>
-			<App/>
-		</XXX>
-	</div>,
-	document.getElementById('root')
-)
-
-// Что нужно написать вместо XXX, YYY и ZZZ, чтобы отобразился список студентов?
+//Какое значение получит переменная winner?
