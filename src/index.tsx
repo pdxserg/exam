faa76f895f361f50017330d94dffcd738e565e76
SergeyStarchenko <pdxserg@gmail.com>
2024-08-01 22:27:24 -0700

    11-3

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,42 @@
-import React, { useCallback, useState } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-export const App = () => {
-	const [temp, setTemp] = useState(100)
+export const TempManager = () => {
+	const [temp, setTemp] = useState(0)
 	const [seconds, setSeconds] = useState(0)
 
-	const resetTemp = useCallback(() => setTemp(0), [])
+	const resetTemp = () => setTemp(0)
+	const increaseSeconds = () => setSeconds(seconds + 100)
 
-	const incSec = useCallback(() => setSeconds(seconds + 1), [])
-
-	return <>
-		<TempDisplay temp={temp} resetTemp={resetTemp}/>
-		<SecDisplay seconds={seconds} incSec={incSec}/>
-	</>
+	return (
+		<>
+			<TempDisplay temp={temp} reset={resetTemp}/>
+			<div>
+				<p><b>Секунды:</b> {seconds} с</p>
+				<button onClick={increaseSeconds}>
+					Увеличить время на 100 секунд
+				</button>
+			</div>
+		</>
+	)
 }
+
 const TempDisplay = React.memo((props: any) => {
 	console.log('Render TempDisplay')
 	return (
-		<div style={{marginBottom: '10px'}} onClick={props.reset}>
-			<p>
-				<b>Температура: </b>{props.temp} &#176;
-			</p>
-			<button onClick={props.resetTemp}>Сбросить температуру к 0</button>
-		</div>
-	)
-})
-
-const SecDisplay = React.memo((props: any) => {
-	console.log('Render SecDisplay')
-	return (
 		<div>
-			<p><b>Секунды:</b> {props.seconds} c </p>
-			<button style={{marginRight: '20px'}}
-			        onClick={props.incSec}>
-				Увеличить время на 1 секунду
-			</button>
+			<p><b>Температура</b>: {props.temp} &#176;</p>
+			<button onClick={props.reset}>Reset</button>
 		</div>
 	)
 })
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<TempManager/>, document.getElementById('root'))
 
-// Почему не корректно работает счетчик времени при нажатии на кнопку (срабатывает только 1 раз) ?
-// Найдите в чем причина.
-// Исправленную версию строки напишите в качестве ответа
 
-// Пример ответа: const incSec = () => setSeconds(seconds + 1)
+//При увеличении времени (при клике на button) компонент TempDisplay
+//тоже перерисовывается. Эта перерисовка является избыточной.
+//Найдите в чем причина лишних перерисовок.
+//Исправленную версию строки напишите в качестве ответа.
+
+//Пример ответа: const increaseSeconds = () => setSeconds(seconds + 100)
