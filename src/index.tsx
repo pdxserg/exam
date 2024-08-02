8db1cd1f8fc964737b63f2ae0007fa67cc703e5e
SergeyStarchenko <pdxserg@gmail.com>
2024-08-01 23:01:56 -0700

    11-5

--- src/index.tsx
+++ src/index.tsx
@@ -1,59 +1,8 @@
-import React, {useMemo, useState} from 'react'
-import ReactDOM from 'react-dom'
+const numbers = [31, 82, 94, 73]
+const mapFunction = (el: number, index: number) => el * index
+const mappedArray = numbers.map(mapFunction)
 
-type ButtonType = {
-	id: number
-	title: string
-	forAdminOnly: boolean
-}
-const buttons: ButtonType[] = [
-	{id: 1, title: 'delete', forAdminOnly: true},
-	{id: 2, title: 'update', forAdminOnly: true},
-	{id: 3, title: 'create', forAdminOnly: false},
-]
+const myNumber = mappedArray[0] && mappedArray[mappedArray.length - 1]
+const bigCount = 41 + myNumber
 
-export const App = ({isAdmin}: { isAdmin: boolean }) => {
-
-	const [seconds, setSeconds] = useState(0)
-
-	const increaseSeconds = () => setSeconds(seconds + 10)
-
-	const correctButtons = useMemo (() => {
-		return buttons.filter(b => isAdmin ? true : !b.forAdminOnly)
-	},[])
-
-	return <>
-		<ButtonsPanel buttons={correctButtons}/>
-		<div>
-			<p>
-				<b>Секунды: {seconds}</b>
-			</p>
-			<button onClick={increaseSeconds}>
-				Увеличить на 10 секунд
-			</button>
-		</div>
-	</>
-}
-
-const ButtonsPanel = React.memo((props: { buttons: Array<ButtonType> }) => {
-	console.log('Render ButtonsPanel')
-	return (
-		<div style={{marginBottom: '15px'}}>
-			<div style={{marginBottom: '15px'}}>
-				<b>Панель с кнопками</b>
-			</div>
-			<div>
-				{props.buttons.map(b => <button key={b.id}>{b.title}</button>)}
-			</div>
-		</div>
-	)
-})
-
-ReactDOM.render(<App isAdmin={true}/>, document.getElementById('root'))
-
-// Что нужно написать вместо XXX и YYY,
-// чтобы избавиться от лишнего перерендера компонента ButtonsPanel
-// при нажатии на кнопку "Увеличить на 10 секунд" ?
-
-// Ответ дайте через пробел: 111 222
-
+//Какое значение получит переменная bigCount?//
