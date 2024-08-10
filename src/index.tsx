ee97a1b0fefaebd5d5e5febfa70fcdf6938b3d00
SergeyStarchenko <pdxserg@gmail.com>
2024-08-09 17:04:36 -0700

    12-4

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,25 @@
 export const reducer = (state: any, action: any) => {
 	switch (action.type) {
-		case 'TRACK-DELETED':
-			return state.filter((track: any) => track.id !== action.trackId)
+		case 'TRACK-ADDED':
+			return {
+				...state,
+				[action.trackId]: {
+					id: action.trackId, likesCount: 0
+				}
+			}
 		default:
 			return state
 	}
 }
 
-const deleteTrackAC =(trackId: number) =>(XXX)
+const addTrackAC = (trackId: number) => ({type: 'TRACK-ADDED', trackId})
 
+const state = {
+	12: {id: 12, likesCount: 10},
+	14: {id: 14, likesCount: 2},
+	100: {id: 100, likesCount: 0},
+}
+const newState = reducer(state, xxx)
+console.log(newState[300].likesCount === 0)
 
-const state = [
-	{id: 12, likesCount: 10},
-	{id: 14, likesCount: 2},
-	{id: 100, likesCount: 0}
-]
-
-const newState = reducer(state, deleteTrackAC(14))
-console.log(newState.length === 2)
-
-// Что нужно написать вместо XXX, чтобы корректно удалить трек и в консоли увидеть true?
+// Что нужно написать вместо XXX, чтобы в консоли увидеть true?
