422cf21272bf9eb5097367064775f8285bf5ce23
SergeyStarchenko <pdxserg@gmail.com>
2024-08-11 17:52:34 -0700

    sprint3-7

--- src/index.tsx
+++ src/index.tsx
@@ -1,66 +1,75 @@
-import React, {useState, useReducer, useEffect} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore} from 'redux'
+import ReactDOM from 'react-dom'
+import {Provider, useSelector, useDispatch} from 'react-redux'
+import React from 'react'
 
-const changeCounter = (state: number, action: any): number => {
+const students = {
+	students: [
+		{id: 1, name: 'Bob'},
+		{id: 2, name: 'Alex'},
+		{id: 3, name: 'Donald'},
+		{id: 4, name: 'Ann'},
+	]
+}
+type RemoveStudentAT = {
+	type: "REMOVE-STUDENT"
+	id: number
+}
+const RemoveStudentAC = (id: number): RemoveStudentAT => ({
+	type: "REMOVE-STUDENT",
+	id
+})
+
+const studentsReducer = (state = students, action: RemoveStudentAT) => {
 	switch (action.type) {
-		case "INC_VALUE":
-			return state + 1
-		case "RESET":
-			return 0
-		case "DEC_VALUE":
-			return state - 1
-		default:
-			return state
+		case "REMOVE-STUDENT":
+			return {
+				...state,
+				students: state.students.filter(s => s.id !== action.id)
+			}
 	}
+	return state
 }
 
-function Counter() {
-	const [value, setValue] = XXX(changeCounter, 0)
-	const [isCounter, setIsCounter] = YYY(true)
-	const commonStyles: React.CSSProperties = {
-		border: "1px solid black",
-		margin: "100px auto",
-		width: "300px",
-		height: "150px",
-		textAlign: "center",
-	}
-	const btnStyles: React.CSSProperties = {
-		color: "white",
-		fontWeight: "bold",
-		backgroundColor: "darkgray",
-		borderRadius: "3px",
-		minWidth: "40px"
-	}
+const store = createStore(studentsReducer)
+type RootStateType = ReturnType<typeof studentsReducer>
 
-	return (
-		<div style={commonStyles}>{
-			isCounter
-				? <div >
-					<div style={{marginBottom: "20px"}}>
-						<h2>{value}</h2>
-						<button
-							style={{...btnStyles, backgroundColor: "red"}}
-							onClick={() => setIsCounter(false)}>OFF</button>
-					</div>
-					<button style={btnStyles} onClick={() => setValue({type: "INC_VALUE"})}>+</button>
-					<button style={btnStyles} onClick={() => setValue({type: "RESET"})}>0</button>
-					<button style={btnStyles} onClick={() => setValue({type: "DEC_VALUE"})}>-</button>
 
-				</div>
-				: <div style={{textAlign: "center"}}>
-					<h2>Counter not working</h2>
-					<button
-						style={{...btnStyles, backgroundColor: "green"}}
-						onClick={() => setIsCounter(true)}>ON</button>
-				</div>
+const StudentList = () => {
+	const listItemStyles = {
+		width: "100px",
+		borderBottom: "1px solid gray",
+		cursor: "pointer",
+	}
+	const students = useSelector((state: RootStateType) => state.students)
+	const dispatch = useDispatch()
+	const studentsList = students.map(s => {
+		const removeStudent = () => {
+			XXX(YYY( ZZZ))
 		}
-		</div>
+		return (
+			<li key={s.id}
+			    style={listItemStyles}
+			    onClick={removeStudent}>
+				{s.name}
+			</li>)
+	})
+	return (
+		<ol>
+			{studentsList}
+		</ol>
+
 	)
 }
 
 
-ReactDOM.render(
-	<Counter/>, document.getElementById('root')
-);
-// Что надо написать вместо XXX и YYY, чтобы код работал? Напишите через пробел.
+ReactDOM.render(<div>
+		<Provider store={store}>
+			<StudentList/>
+		</Provider>
+	</div>,
+	document.getElementById('root')
+)
+
+// Что нужно написать вместо XXX, YYY и ZZZ, чтобы при клике по имени студент
+// удалялся из списка? Напишите через пробел.
