c163a9420495b456095d53f9173e969034e11946
SergeyStarchenko <pdxserg@gmail.com>
2024-08-11 17:35:47 -0700

    sprint3-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,34 @@
-const value = 33
-
-const getValue = (value: any) => {
-	value += 84
-	return value
+type Status = 'Stopped' | 'Playing' | 'Paused'
+type StateType = {
+	volume: number // in percents
+	trackUrl: string // 'https://blabla.com/track01.mp3',
+	currentPlayPosition: number // milliseconds,
+	status: Status
+}
+export const playerReducer = (state: StateType, action: any) => {
+	switch (action.type) {
+		case 'TRACK-VOLUME-CHANGED':
+			return {
+				...state,
+				XXX
+			}
+		default:
+			return state
+	}
 }
 
-const myResult= getValue("") || value
+const muteTrackAC = () => ({type: 'TRACK-MUTED'})
+const changeVolumeAC = (volumeLevel: number) => ({type: 'TRACK-VOLUME-CHANGED', volumeLevel})
+const changeTrackAC = (url: string) => ({type: 'TRACK-URL-CHANGED', url})
+const changeTrackPlayStatusAC = (status: Status) => ({type: 'TRACK-STATUS-CHANGED', status})
 
-//Какое значение получит переменная myResult?
+const state: StateType = {
+	status: 'Stopped',
+	currentPlayPosition: 1213,
+	trackUrl: 'https://blabla.com/track01.mp3',
+	volume: 100
+}
+const newState = playerReducer(state, changeVolumeAC(20))
+console.log(newState.volume === 20)
+
+// Напишите вместо XXX правильную строку кода, чтобы изменить громкость трека и увидеть в консоли true.
