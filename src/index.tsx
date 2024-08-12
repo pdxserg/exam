d4f95cda2f8aaaed649d49958a3f3a283b8c73be
SergeyStarchenko <pdxserg@gmail.com>
2024-08-11 17:49:41 -0700

    sprint3-6

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,66 @@
-import {combineReducers, createStore} from 'redux'
+import React, {useState, useReducer, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-let initialState = {items: [{name: 'Dimych'}, {name: 'Ignat'}]}
-const usersReducer = (state = initialState, action: any) => {
-	return state
+const changeCounter = (state: number, action: any): number => {
+	switch (action.type) {
+		case "INC_VALUE":
+			return state + 1
+		case "RESET":
+			return 0
+		case "DEC_VALUE":
+			return state - 1
+		default:
+			return state
+	}
 }
 
-const store = createStore(combineReducers({
-	users: usersReducer
-}))
+function Counter() {
+	const [value, setValue] = XXX(changeCounter, 0)
+	const [isCounter, setIsCounter] = YYY(true)
+	const commonStyles: React.CSSProperties = {
+		border: "1px solid black",
+		margin: "100px auto",
+		width: "300px",
+		height: "150px",
+		textAlign: "center",
+	}
+	const btnStyles: React.CSSProperties = {
+		color: "white",
+		fontWeight: "bold",
+		backgroundColor: "darkgray",
+		borderRadius: "3px",
+		minWidth: "40px"
+	}
 
-store.XXX(() => {
-	console.log('state changed')
-})
+	return (
+		<div style={commonStyles}>{
+			isCounter
+				? <div >
+					<div style={{marginBottom: "20px"}}>
+						<h2>{value}</h2>
+						<button
+							style={{...btnStyles, backgroundColor: "red"}}
+							onClick={() => setIsCounter(false)}>OFF</button>
+					</div>
+					<button style={btnStyles} onClick={() => setValue({type: "INC_VALUE"})}>+</button>
+					<button style={btnStyles} onClick={() => setValue({type: "RESET"})}>0</button>
+					<button style={btnStyles} onClick={() => setValue({type: "DEC_VALUE"})}>-</button>
 
-store.dispatch({type: 'ANY'})
+				</div>
+				: <div style={{textAlign: "center"}}>
+					<h2>Counter not working</h2>
+					<button
+						style={{...btnStyles, backgroundColor: "green"}}
+						onClick={() => setIsCounter(true)}>ON</button>
+				</div>
+		}
+		</div>
+	)
+}
 
-// Что нужно написать вместо XXX, чтобы в консоли увидеть 'state changed'?
+
+ReactDOM.render(
+	<Counter/>, document.getElementById('root')
+);
+// Что надо написать вместо XXX и YYY, чтобы код работал? Напишите через пробел.
