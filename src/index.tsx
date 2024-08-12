1eecb466cc729f2eeeff22411151ea188786f319
SergeyStarchenko <pdxserg@gmail.com>
2024-08-11 17:41:14 -0700

    sprint3-3

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,59 @@
-export const reducer = (state: any, action: any) => {
-	switch (action.type) {
-		case 'TRACK-DELETED':
-			return state.filter((track: any) => XXX)
-		default:
-			return state
-	}
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+type ButtonType = {
+	id: number
+	title: string
+	forAdminOnly: boolean
+}
+const buttons: ButtonType[] = [
+	{id: 1, title: 'delete', forAdminOnly: true},
+	{id: 2, title: 'update', forAdminOnly: true},
+	{id: 3, title: 'create', forAdminOnly: false},
+]
+
+export const App = ({isAdmin}: { isAdmin: boolean }) => {
+
+	const [seconds, setSeconds] = useState(0)
+
+	const increaseSeconds = () => setSeconds(seconds + 10)
+
+	const correctButtons = XXX(() => {
+		return buttons.filter(b => isAdmin ? true : !b.forAdminOnly)
+	}, [YYY])
+
+	return <>
+		<ButtonsPanel buttons={correctButtons}/>
+		<div>
+			<p>
+				<b>Секунды: {seconds}</b>
+			</p>
+			<button onClick={increaseSeconds}>
+				Увеличить на 10 секунд
+			</button>
+		</div>
+	</>
 }
 
-const deleteTrackAC = (trackId: number) => ({type: 'TRACK-DELETED', trackId})
+const ButtonsPanel = React.memo((props: { buttons: Array<ButtonType> }) => {
+	console.log('Render ButtonsPanel')
+	return (
+		<div style={{marginBottom: '15px'}}>
+			<div style={{marginBottom: '15px'}}>
+				<b>Панель с кнопками</b>
+			</div>
+			<div>
+				{props.buttons.map(b => <button key={b.id}>{b.title}</button>)}
+			</div>
+		</div>
+	)
+})
 
+ReactDOM.render(<App isAdmin={true}/>, document.getElementById('root'))
 
-const state = [
-	{id: 12, likesCount: 10},
-	{id: 14, likesCount: 2},
-	{id: 100, likesCount: 0}
-]
-const newState = reducer(state, deleteTrackAC(14))
+// Что нужно написать вместо XXX и YYY,
+// чтобы избавиться от лишнего перерендера компонента ButtonsPanel
+// при нажатии на кнопку "Увеличить на 10 секунд" ?
 
-console.log(newState.length === 2)
+// Ответ дайте через пробел: 111 222
 
-
-// Что нужно написать вместо XXX, чтобы корректно удалить трек и в консоли увидеть true?
