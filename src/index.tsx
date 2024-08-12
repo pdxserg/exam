a2ec9d4a2c35360b463b3eace855de5a9181d038
SergeyStarchenko <pdxserg@gmail.com>
2024-08-11 17:46:18 -0700

    sprint3-5

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,18 @@
-import React, { useCallback, useState } from 'react'
-import ReactDOM from 'react-dom'
+import {combineReducers, createStore} from 'redux'
 
-export const App = () => {
-	const [temp, setTemp] = useState(100)
-	const [seconds, setSeconds] = useState(0)
-
-	const resetTemp = useCallback(() => setTemp(0), [])
-
-	const incSec = useCallback(() => setSeconds(seconds + 1), [])
-
-	return <>
-		<TempDisplay temp={temp} resetTemp={resetTemp}/>
-		<SecDisplay seconds={seconds} incSec={incSec}/>
-	</>
+let initialState = {items: [{name: 'Dimych'}, {name: 'Ignat'}]}
+const usersReducer = (state = initialState, action: any) => {
+	return state
 }
-const TempDisplay = React.memo((props: any) => {
-	console.log('Render TempDisplay')
-	return (
-		<div style={{marginBottom: '10px'}} onClick={props.reset}>
-			<p>
-				<b>Температура: </b>{props.temp} &#176;
-			</p>
-			<button onClick={props.resetTemp}>Сбросить температуру к 0</button>
-		</div>
-	)
+
+const store = createStore(combineReducers({
+	users: usersReducer
+}))
+
+store.XXX(() => {
+	console.log('state changed')
 })
 
-const SecDisplay = React.memo((props: any) => {
-	console.log('Render SecDisplay')
-	return (
-		<div>
-			<p><b>Секунды:</b> {props.seconds} c </p>
-			<button style={{marginRight: '20px'}}
-			        onClick={props.incSec}>
-				Увеличить время на 1 секунду
-			</button>
-		</div>
-	)
-})
+store.dispatch({type: 'ANY'})
 
-ReactDOM.render(<App/>, document.getElementById('root'))
-
-// Почему не корректно работает счетчик времени при нажатии на кнопку (срабатывает только 1 раз) ?
-// Найдите в чем причина.
-// Исправленную версию строки напишите в качестве ответа
-
-// Пример ответа: const incSec = () => setSeconds(seconds + 1)
+// Что нужно написать вместо XXX, чтобы в консоли увидеть 'state changed'?
