a5bd6d1d2b063f4627b643abee6e392a37fa6d94
SergeyStarchenko <pdxserg@gmail.com>
2024-10-09 22:02:04 -0700

    sprint 4-4-5 again

--- src/index.tsx
+++ src/index.tsx
@@ -1,88 +1,33 @@
-import { useFormik } from 'formik';
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-
-// Main
-export const Login = () => {
-
-	const formik = useFormik({
-		initialValues: {
-			firstName: '',
-			lastName: '',
-			email: '',
-			password: '',
-			phone: '',
-		},
-		onSubmit: values => {
-			alert(JSON.stringify(values, null, 2));
-		},
-	});
-
+export const Main = () => {
 	return (
-		<form onSubmit={formik.handleSubmit}>
-			<div>
-				<input
-					name="firstName"
-					onChange={formik.handleChange}
-					value={formik.values.firstName}
-					placeholder={'Введите имя'}
-				/>
-			</div>
-			<div>
-				<input
-					name="lastName"
-					onChange={formik.handleChange}
-					value={formik.values.lastName}
-					placeholder={'Введите фамилию'}
-				/>
-			</div>
-			<div>
-				<input
-					name="email"
-					onChange={formik.handleChange}
-					value={formik.values.email}
-					placeholder={'Введите email'}
-				/>
-			</div>
-			<div>
-				<input
-					name="password"
-					onChange={formik.handleChange}
-					value={formik.values.password}
-					placeholder={'Введите пароль'}
-					type={'password'}
-				/>
-			</div>
-			<div>
-				<input
-					name="password"
-					onChange={formik.handleChange}
-					value={formik.values.phone}
-					placeholder={'Введите телефон'}
-				/>
-			</div>
-			<button type="submit">Отправить</button>
-		</form>
-	);
+		<>
+			<h2>✅ Список тудулистов</h2>
+			<h2>📜 Список постов</h2>
+		</>
+	)
 }
 
 // App
 export const App = () => {
+
 	return (
 		<Routes>
-			<Route path={''} element={<Login/>}/>
+			<Route path={'/'} element={Main}/>
 		</Routes>
 	)
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<BrowserRouter><App/></BrowserRouter>)
 
 // 📜 Описание:
-// Форма заполнения данных работает некорректно.
-// Пользователи жалуются на поле ввода "Телефона"
-// Найдите в коде ошибку. Исправленную версию строки напишите в качестве ответа.
+// Приложение при старте падает с ошибкой...
+// Найдите и исправьте ошибку, чтобы на экране отобразилось 2 заголовка.
+// Исправленную версию строки напишите в качестве ответа.
 
-// 🖥 Пример ответа: <form onSubmit={formik.handleSubmit}>
+// 🖥 Пример ответа: type InitStateType = typeof initState
