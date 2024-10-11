e39ead3f842b6eb73a7496b4aaaa9304e0ef9b60
SergeyStarchenko <pdxserg@gmail.com>
2024-10-10 21:04:27 -0700

    sprint 4-5-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,166 +1,96 @@
-import { useFormik } from "formik";
-import React from "react";
-import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import axios from "axios";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 // Types
-type LoginFieldsType = {
+type CommentType = {
+	postId: string;
+	id: string;
+	name: string;
 	email: string;
-	password: string;
+	body: string;
 };
 
-// API
+// Api
 const instance = axios.create({ baseURL: "https://exams-frontend.kimitsu.it-incubator.io/api/" });
 
-const api = {
-	login(data: LoginFieldsType) {
-		return instance.post("auth/login", data);
+const commentsAPI = {
+	getComments() {
+		return instance.get<CommentType[]>("comments");
 	},
 };
 
 // Reducer
-const initState = {
-	isLoading: false,
-	error: null as string | null,
-	isLoggedIn: false,
-};
+const initState = [] as CommentType[];
 
 type InitStateType = typeof initState;
 
-const appReducer = (state: InitStateType = initState, action: ActionsType): InitStateType => {
+const commentsReducer = (state: InitStateType = initState, action: ActionsType): InitStateType => {
 	switch (action.type) {
-		case "APP/SET-IS-LOGGED-IN":
-			return { ...state, isLoggedIn: action.isLoggedIn };
-		case "APP/IS-LOADING":
-			return { ...state, isLoading: action.isLoading };
-		case "APP/SET-ERROR":
-			return { ...state, error: action.error };
+		case "COMMENTS/GET-COMMENTS":
+			return action.comments;
 		default:
 			return state;
 	}
 };
 
-// Actions
-const setIsLoggedIn = (isLoggedIn: boolean) =>
-	({ type: "APP/SET-IS-LOGGED-IN", isLoggedIn }) as const;
-const setLoadingAC = (isLoading: boolean) => ({ type: "APP/IS-LOADING", isLoading }) as const;
-const setError = (error: string | null) => ({ type: "APP/SET-ERROR", error }) as const;
-type ActionsType =
-	| ReturnType<typeof setIsLoggedIn>
-	| ReturnType<typeof setLoadingAC>
-	| ReturnType<typeof setError>;
+const getCommentsAC = (comments: CommentType[]) =>
+	({ type: "COMMENTS/GET-COMMENTS", comments }) as const;
+type ActionsType = ReturnType<typeof getCommentsAC>;
 
-// Thunk
-const loginTC =
-	(values: LoginFieldsType): AppThunk =>
-		(dispatch) => {
-			dispatch(setLoadingAC(true));
-			api
-				.login(values)
-				.then((res) => {
-					dispatch(setIsLoggedIn(true));
-					alert("Вы залогинились успешно");
-				})
-				.catch((e) => {
-					dispatch(setError(e.response.data.errors));
-				})
-				.finally(() => {
-					dispatch(setLoadingAC(false));
-					setTimeout(() => {
-						dispatch(setError(null));
-					}, 3000);
-				});
-		};
+const getCommentsTC = (): ThunkAction<any, any, any, any> => (dispatch) => {
+	commentsAPI.getComments().then((res) => {
+		dispatch(getCommentsAC(res.data));
+	});
+};
 
 // Store
 const rootReducer = combineReducers({
-	app: appReducer,
+	comments: commentsReducer,
 });
 
 const store = configureStore({ reducer: rootReducer });
 type RootState = ReturnType<typeof store.getState>;
 type AppDispatch = ThunkDispatch<RootState, unknown, ActionsType>;
-type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, ActionsType>;
 const useAppDispatch = () => useDispatch<AppDispatch>();
 const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-// Loader
-export const Loader = () => {
-	return <h1>Loading ...</h1>;
-};
-
-// Profile
-export const Profile = () => {
-	return <h2>😎 Profile</h2>;
-};
-
-// Login
-export const Login = () => {
-	const dispatch = useAppDispatch();
-	const navigate = useNavigate();
-
-	const error = useAppSelector((state) => state.app.error);
-	const isLoading = useAppSelector((state) => state.app.isLoading);
-	const isLoggedIn = useAppSelector((state) => state.app.isLoggedIn);
-
-	const formik = useFormik({
-		initialValues: {
-			email: "darrell@gmail.com",
-			password: "123",
-		},
-		onSubmit: (values) => {
-			dispatch(loginTC(values));
-		},
-	});
-
-	return (
-		<div>
-			{!!error && <h2 style={{ color: "red" }}>{error}</h2>}
-			{isLoading && <Loader />}
-			<form onSubmit={formik.handleSubmit}>
-				<div>
-					<input placeholder={"Введите email"} {...formik.getFieldProps("email")} />
-				</div>
-				<div>
-					<input
-						type={"password"}
-						placeholder={"Введите пароль"}
-						{...formik.getFieldProps("password")}
-					/>
-				</div>
-				<button type="submit">Залогиниться</button>
-			</form>
-		</div>
-	);
-};
-
 // App
 export const App = () => {
+	const comments = useAppSelector((state) => state.comments);
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(getCommentsTC());
+	}, []);
+
 	return (
-		<Routes>
-			<Route path={""} element={<Login />} />
-			<Route path={"profile"} element={<Profile />} />
-		</Routes>
+		<>
+			<h1>📝 Список комментариев</h1>
+			{comments.map((c) => {
+				return (
+					<div key={c.id}>
+						<b>Comment</b>: {c.body}{" "}
+					</div>
+				);
+			})}
+		</>
 	);
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<Provider store={store}>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<App />
 	</Provider>,
 );
 
 // 📜 Описание:
-// ❗ Email и password менять не надо. Это просто тестовые данные с которыми будет происходить успешный запрос.
-// Нажмите на кнопку "Залогиниться" и вы увидели alert с успешным сообщением
-// Задача: при успешной логинизации, редиректнуть пользователя на страницу Profile.
+// Ваша задача стоит в том чтобы правильно передать нужные типы в дженериковый тип ThunkAction<any, any, any, any>.
+// Что нужно написать вместо any, any, any, any чтобы правильно типизировать thunk creator?
+// Ответ дайте через пробел
 
-// Напишите строку кода, которую нужно добавить
-// 🖥 Пример ответа: if (true) { console.log('hello') }
+// 🖥 Пример ответа: unknown status isDone void
