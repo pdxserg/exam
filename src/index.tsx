52cb31064daed133acf78ac543e6f171f16478d4
SergeyStarchenko <pdxserg@gmail.com>
2024-10-10 21:01:20 -0700

    sprint 4-5-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,62 +1,80 @@
-import React, { useEffect } from "react";
+import { useFormik } from "formik";
+import React from "react";
 import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
-// Utils
-console.log = () => {};
+// Types
+type LoginFieldsType = {
+	email: string;
+	password: string;
+};
 
-// Api
-const instance = axios.create({
-	baseURL: "xxx",
-});
+// API
+const instance = axios.create({ baseURL: "https://exams-frontend.kimitsu.it-incubator.io/api/" });
 
 const api = {
-	getUsers() {
-		return instance.get("xxx");
+	login(data: LoginFieldsType) {
+		return instance.post("auth/login", data);
 	},
 };
 
 // Reducer
 const initState = {
 	isLoading: false,
-	users: [] as any[],
+	error: null as string | null,
+	isLoggedIn: false,
 };
 
 type InitStateType = typeof initState;
 
 const appReducer = (state: InitStateType = initState, action: ActionsType): InitStateType => {
 	switch (action.type) {
-		case "APP/SET-USERS":
-			/* 1 */
-			return { ...state, users: action.users };
+		case "APP/SET-IS-LOGGED-IN":
+			return { ...state, isLoggedIn: action.isLoggedIn };
 		case "APP/IS-LOADING":
-			/* 2 */
 			return { ...state, isLoading: action.isLoading };
+		case "APP/SET-ERROR":
+			return { ...state, error: action.error };
 		default:
 			return state;
 	}
 };
 
 // Actions
-const setUsersAC = (users: any[]) => ({ type: "APP/SET-USERS", users }) as const;
+const setIsLoggedIn = (isLoggedIn: boolean) =>
+	({ type: "APP/SET-IS-LOGGED-IN", isLoggedIn }) as const;
 const setLoadingAC = (isLoading: boolean) => ({ type: "APP/IS-LOADING", isLoading }) as const;
-type ActionsType = ReturnType<typeof setUsersAC> | ReturnType<typeof setLoadingAC>;
+const setError = (error: string | null) => ({ type: "APP/SET-ERROR", error }) as const;
+type ActionsType =
+	| ReturnType<typeof setIsLoggedIn>
+	| ReturnType<typeof setLoadingAC>
+	| ReturnType<typeof setError>;
 
 // Thunk
-const getUsersTC = (): AppThunk => (dispatch) => {
-	/* 3 */
-	dispatch(setLoadingAC(true));
-	api.getUsers().then((res) => {
-		/* 4 */
-		dispatch(setLoadingAC(false));
-		/* 5 */
-		dispatch(setUsersAC(res.data.data));
-	});
-};
+const loginTC =
+	(values: LoginFieldsType): AppThunk =>
+		(dispatch) => {
+			dispatch(setLoadingAC(true));
+			api
+				.login(values)
+				.then((res) => {
+					dispatch(setIsLoggedIn(true));
+					alert("Вы залогинились успешно");
+				})
+				.catch((e) => {
+					dispatch(setError(e.response.data.errors));
+				})
+				.finally(() => {
+					dispatch(setLoadingAC(false));
+					setTimeout(() => {
+						dispatch(setError(null));
+					}, 3000);
+				});
+		};
 
 // Store
 const rootReducer = combineReducers({
@@ -72,46 +90,60 @@ const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 // Loader
 export const Loader = () => {
-	/* 6 */
 	return <h1>Loading ...</h1>;
+};
+
+// Profile
+export const Profile = () => {
+	return <h2>😎 Profile</h2>;
 };
 
 // Login
 export const Login = () => {
-	/* 7 */
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
 
-	const users = useAppSelector((state) => state.app.users);
+	const error = useAppSelector((state) => state.app.error);
 	const isLoading = useAppSelector((state) => state.app.isLoading);
+	const isLoggedIn = useAppSelector((state) => state.app.isLoggedIn);
+
+	const formik = useFormik({
+		initialValues: {
+			email: "darrell@gmail.com",
+			password: "123",
+		},
+		onSubmit: (values) => {
+			dispatch(loginTC(values));
+		},
+	});
 
 	return (
 		<div>
+			{!!error && <h2 style={{ color: "red" }}>{error}</h2>}
 			{isLoading && <Loader />}
-			{users.map((u) => (
-				<p key={u.id}>{u.email}</p>
-			))}
-			<h1>
-				В данном задании на экран смотреть не нужно. Рекомендуем взять ручку, листик и
-				последовательно, спокойно расставить цифры в нужном порядке. Прежде чем давать ответ
-				обязательно посчитайте к-во цифр и сверьте с подсказкой. Удачи 🚀
-			</h1>
+			<form onSubmit={formik.handleSubmit}>
+				<div>
+					<input placeholder={"Введите email"} {...formik.getFieldProps("email")} />
+				</div>
+				<div>
+					<input
+						type={"password"}
+						placeholder={"Введите пароль"}
+						{...formik.getFieldProps("password")}
+					/>
+				</div>
+				<button type="submit">Залогиниться</button>
+			</form>
 		</div>
 	);
 };
 
 // App
 export const App = () => {
-	/* 8 */
-	const dispatch = useAppDispatch();
-
-	useEffect(() => {
-		/* 9 */
-		dispatch(getUsersTC());
-	}, []);
-
-	/* 10 */
 	return (
 		<Routes>
 			<Route path={""} element={<Login />} />
+			<Route path={"profile"} element={<Profile />} />
 		</Routes>
 	);
 };
@@ -126,8 +158,9 @@ root.render(
 );
 
 // 📜 Описание:
-// Задача: напишите в какой последовательности вызовутся числа при успешном запросе.
-// Подсказка: будет 13 чисел.
-// Ответ дайте через пробел.
+// ❗ Email и password менять не надо. Это просто тестовые данные с которыми будет происходить успешный запрос.
+// Нажмите на кнопку "Залогиниться" и вы увидели alert с успешным сообщением
+// Задача: при успешной логинизации, редиректнуть пользователя на страницу Profile.
 
-// 🖥 Пример ответа: 1 2 3 4 5 6 7 8 9 10 1 2 3
+// Напишите строку кода, которую нужно добавить
+// 🖥 Пример ответа: if (true) { console.log('hello') }
