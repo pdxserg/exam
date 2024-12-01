35c7a7eb4ad229b297c6b462efdec8c4d27d1e86
SergeyStarchenko <pdxserg@gmail.com>
2024-12-01 10:39:28 -0800

    sprint 5-7

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,20 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
-
-const Login = () => {
-	const navigate = useNavigate()
-
-	useEffect(() => {
-		navigate('/secret/JIUzI1NiIsInR5cCI6IkpXVCJ9')
-	}, [])
-
-	return (
-		<div>Login</div>
-	)
-}
-const SecretToken = () => {
-	const token = 'no token' // FIX
-
-	return (
-		<h1>🦾 token: {token}</h1>
-	)
-}
 
 export const App = () => {
 	return (
-		<Routes>
-			<Route path={'/'} element={<Login/>}/>
-			<Route path={'/secret/:token'} element={<SecretToken/>}/>
-		</Routes>
+		<div>
+			<h2>Что нужно прописать в консоли, чтобы создать новую ветку с названием login и перейти на нее ?</h2>
+		</div>
 	)
 }
 
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-	<BrowserRouter>
-		<App/>
-	</BrowserRouter>
-);
+root.render(<App/>);
 
-// 📜Описание:
-// Исправьте код на 17 строке так, чтобы на странице отобразился токен.
+// 📜 Описание:
+// Что нужно прописать в консоли, чтобы создать новую ветку с названием login и перейти на нее ?
+//❗Ответ написать одной командой
 
-//❗Ответ можно указывать с типизацией и без. Учтено несколько вариантов
-// 🖥 Пример ответа: const token = '123'
+// 🖥 Пример ответа: git create login
