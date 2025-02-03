12ae6065ab01231c5f118ff3214fc6f1b681c785
SergeyStarchenko <pdxserg@gmail.com>
2025-02-03 15:51:37 -0800

    sprint 3-3-3

--- src/index.tsx
+++ src/index.tsx
@@ -1,67 +1,13 @@
-import React from 'react'
-import { createStore } from 'redux'
-import { Provider, useSelector, useDispatch } from 'react-redux'
-import ReactDOM from 'react-dom'
-
-type StudentType = {
-	id: number
-	name: string
-	age: number
+const control = {
+	name: "Marina",
+	age: 39,
+	technologies: ["HTML", "", "REACT"]
 }
 
-const initState = {
-	students:
-		[
-			{id: 1, name: 'Bob', age: 23},
-			{id: 2, name: 'Alex', age: 22}
-		] as Array<StudentType>
-}
-type AddStudentAT = {
-	type: 'ADD-STUDENT'
-	name: string
-	age: number
-	id: number
+const [science = "FOO", presence = "BOO", friendship = "DOO"] = control.technologies
+
+if(!XXX){
+	alert("HEY")
 }
 
-type InitialStateType = typeof initState
-
-const studentsReducer = (state: InitialStateType = initState, action: AddStudentAT): InitialStateType => {
-	switch (action.type) {
-		case 'ADD-STUDENT':
-			return {
-				...state,
-				students: [...state.students, {
-					name: action.name,
-					age: action.age,
-					id: action.id
-				}]
-			}
-	}
-	return state
-}
-
-const appStore = createStore(studentsReducer)
-type RootStateType = ReturnType<typeof studentsReducer>
-
-
-const StudentList = () => {
-	const students = useSelector((state: RootStateType) => state.students)
-	return (
-		<ul>
-			{students.map(s => <li key={s.id}>{`${s.name}. ${s.age} years.`}</li>)}
-		</ul>
-	)
-}
-const App = () => {
-	return <StudentList/>
-}
-
-ReactDOM.render(<div>
-		<XXX YYY={ZZZ}>
-			<App/>
-		</XXX>
-	</div>,
-	document.getElementById('root')
-)
-
-// Что нужно написать вместо XXX, YYY и ZZZ, чтобы отобразился список студентов?
+// Какую переменную следует указать вместо XXX, что бы была вызвана функция alert?
