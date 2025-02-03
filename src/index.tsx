9a6a6973203da7e2fecc862b62645132cb4f0988
SergeyStarchenko <pdxserg@gmail.com>
2025-02-03 15:57:28 -0800

    sprint 3-3-4

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,36 @@
-const control = {
-	name: "Marina",
-	age: 39,
-	technologies: ["HTML", "", "REACT"]
+type StateType = {
+	volume: number // in percents
+	trackUrl: string // 'https://blabla.com/track01.mp3',
+	currentPlayPosition: number // milliseconds,
 }
 
-const [science = "FOO", presence = "BOO", friendship = "DOO"] = control.technologies
-
-if(!XXX){
-	alert("HEY")
+export const reducer = (state: StateType, action: any) => {
+	switch (action.type) {
+		case XXX:
+			return {
+				...state,
+				trackUrl: action.url
+			}
+		case YYY:
+			return {
+				...state,
+				volume: 0
+			}
+		case ZZZ:
+			return {
+				...state,
+				currentPlayPosition: 0
+			}
+		default:
+			return state
+	}
 }
 
-// Какую переменную следует указать вместо XXX, что бы была вызвана функция alert?
+const muteTrackAC = () => ({type: 'TRACK-MUTED'})
+const changeTrackAC = (url: string) => ({type: 'TRACK-URL-CHANGED', url})
+// перемотатьНаНачало:
+const rewindToStart = () => ({type: 'TRACK-REWOUND-TO-START'})
+
+// Какие типы должны быть вместо XXX, YYY и ZZZ?
+// Ответ дать через пробел, например:   'BLABLA' 'HEYНЕY' 'HIPHOP'
+вызвана функция alert?
