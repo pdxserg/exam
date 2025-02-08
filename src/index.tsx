b7519bc70f1bf94a910196ba22d5a34fd9d0dfb3
SergeyStarchenko <pdxserg@gmail.com>
2025-02-08 09:37:46 -0800

    sprint 5-1-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,69 @@
-export const reducer = (state: any, action: any) => {
-	switch (action.type) {
-		case 'TRACK-ADDED':
-			return XXX
-		default:
-			return state
-	}
-}
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { createRoot } from "react-dom/client";
+import { Provider, useDispatch, useSelector } from "react-redux";
 
-const addTrackAC = (trackId: number) => ({type: 'TRACK-ADDED', trackId})
+// slice
+const slice = createSlice({
+	name: "classroom",
+	initialState: {
+		students: [
+			{ id: 1, name: "Alice" },
+			{ id: 2, name: "Bob" },
+			{ id: 3, name: "Charlie" },
+		],
+	},
+	reducers: {
+		removeStudent: (state, action) => {
+			return state;
+		},
+	},
+});
 
+const { removeStudent } = slice.actions;
 
-const state = [
-	{id: 12, likesCount: 10},
-	{id: 14, likesCount: 2},
-	{id: 100, likesCount: 0}
-]
-const newState = reducer(state, addTrackAC(300))
+// App.tsx
+const App = () => {
+	const students = useSelector((state: RootState) => state.classroom.students);
+	const dispatch = useDispatch();
 
-console.log(newState[3].likesCount === 0)
+	const handleRemove = (id: number) => {
+		dispatch(removeStudent(id));
+	};
 
-// Что нужно написать вместо XXX, чтобы трек корректно добавился и в консоли увидеть true?
+	return (
+		<ul>
+			{students.map((student) => (
+				<li key={student.id}>
+					{student.name}
+					<button onClick={() => handleRemove(student.id)}>✖</button>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+// store.ts
+export const store = configureStore({
+	reducer: {
+		classroom: slice.reducer,
+	},
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+// main.ts
+createRoot(document.getElementById("root")!).render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+);
+
+// 📜 Описание:
+// При нажатии на кнопку ✖ рядом с именем студента, студент не удаляется из списка 🥲
+
+// 🪛 Задача:
+// Перепишите изменение стейта таким образом, чтобы при нажатии на кнопку ✖, студент удалялся из списка.
+// В качестве ответа укажите исправленную строку кода.
+// ❗Изменение стейта должно быть написано мутабельным образом
+// ❗Не используйте деструктуризацию action.payload (const {id} = action.payload)
+// ❗Не создавайте переменные из action.payload (const id = action.payload.id)
