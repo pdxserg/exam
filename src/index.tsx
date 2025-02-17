62ef7e909a44aec4e1a2ff6320123776f80d4f47
SergeyStarchenko <pdxserg@gmail.com>
2025-02-17 10:12:19 -0800

    sprint 5-2-7

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,81 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client';
+import { configureStore } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
-export const App = () => {
+type Product = {
+	id: string;
+	title: string;
+	description: string;
+	price: number;
+};
+
+export type Response = {
+	total: number;
+	messages: string[];
+	page: number;
+	pageCount: number;
+	data: Product[];
+};
+
+// Api
+const productsApi = createApi({
+	reducerPath: "productsApi",
+	baseQuery: fetchBaseQuery({ baseUrl: "https://exams-frontend.kimitsu.it-incubator.io/api/" }),
+	endpoints: (builder) => {
+		return {
+			getProducts: builder.query<Product[], void>({
+				query: () => {
+					return {
+						method: "GET",
+						url: "products",
+					};
+				},
+				// ❗❗❗XXX❗❗❗
+			}),
+		};
+	},
+});
+
+const { useGetProductsQuery } = productsApi;
+
+// App.tsx
+const App = () => {
+	const { data: products } = useGetProductsQuery();
+
 	return (
-		<div>
-			<h2>Какая команда позволяет на время «сдать в архив» (или отложить) изменения, сделанные в рабочей
-				копии, чтобы вы могли применить их позже? Откладывание изменений полезно, если вам необходимо переключить
-				контекст и вы пока не готовы к созданию коммита.</h2>
-		</div>
-	)
-}
+		<>
+			{products?.map((el) => {
+				return (
+					<div key={el.id} style={{ border: "1px solid", margin: "5px", padding: "5px" }}>
+						<p>title - {el.title}</p>
+						<p>description - {el.description}</p>
+					</div>
+				);
+			})}
+		</>
+	);
+};
 
+// store.ts
+const store = configureStore({
+	reducer: {
+		[productsApi.reducerPath]: productsApi.reducer,
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productsApi.middleware),
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App/>);
+createRoot(document.getElementById("root")!).render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+);
 
 // 📜 Описание:
-// Какая команда позволяет на время «сдать в архив» (или отложить) изменения, сделанные в рабочей
-// копии, чтобы вы могли применить их позже? Откладывание изменений полезно, если вам необходимо переключить
-// контекст и вы пока не готовы к созданию коммита.
+// Белый экран. Откройте панель разработчика и проанализируйте в чем ошибка
 
-// 🖥 Пример ответа: git init
+// 🪛 Задача:
+// Что нужно написать вместо  `// ❗❗❗XXX❗❗❗` чтобы на экране отобразились продукты
+// В качестве ответа укажите написанный вами код
+// ❗Типизацию указывать обязательно
+// ❗Ответ принимает синтаксис стрелочной функции
