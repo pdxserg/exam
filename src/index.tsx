cc4a576d53fadd3107ab4199d1daa085ab6800ea
SergeyStarchenko <pdxserg@gmail.com>
2025-02-20 20:52:51 -0800

    sprint 5-3-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,81 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 
-type Comment = {
-	postId: string;
+type User = {
 	id: string;
 	name: string;
-	email: string;
-	body: string;
+	age: number;
+};
+
+type UsersResponse = {
+	items: User[];
+	totalCount: number;
 };
 
 // Api
 const api = createApi({
-	reducerPath: "commentsApi",
+	reducerPath: "api",
 	baseQuery: fetchBaseQuery({ baseUrl: "https://exams-frontend.kimitsu.it-incubator.io/api/" }),
 	endpoints: (builder) => {
 		return {
-			// ❗❗❗XXX❗❗❗
+			getUsers: builder.query<UsersResponse, void>({
+				query: () => "users",
+			}),
 		};
 	},
 });
 
-const { useGetCommentsQuery } = api;
+const { useGetUsersQuery } = api;
 
-// App.tsx
-const App = () => {
-	const { data } = useGetCommentsQuery();
+// Users.tsx
+const Users = () => {
+	const { data } = useGetUsersQuery();
+
+	const dispatch = useAppDispatch();
+
+	const addSmileHandler = (id: string) => {
+		const smile = "😁";
+		// ❗❗❗XXX❗❗❗
+	};
 
 	return (
 		<>
-			{data?.map((el) => {
-				return (
-					<div key={el.id} style={{ border: "1px solid", margin: "5px", padding: "5px" }}>
-						<p>body - {el.body}</p>
-					</div>
-				);
-			})}
+			<h1>Users</h1>
+			{data?.items.map((el) => (
+				<div key={el.id}>
+					name - <b>{el.name}</b>
+					<button onClick={() => addSmileHandler(el.id)}>Add smile</button>
+				</div>
+			))}
 		</>
 	);
 };
 
 // store.ts
 const store = configureStore({
-	reducer: {
-		[api.reducerPath]: api.reducer,
-	},
+	reducer: { [api.reducerPath]: api.reducer },
 	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
 
+type AppDispatch = typeof store.dispatch;
+const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+
 createRoot(document.getElementById("root")!).render(
 	<Provider store={store}>
-		<App />
+		<Users />
 	</Provider>,
 );
 
 // 📜 Описание:
-// Белый экран. Откройте панель разработчика и проанализируйте в чем ошибка
-
+// Откройте redux devtools и убедитесь, что данные из запроса хранятся в кеше
+// http://surl.li/veofpd
 // 🪛 Задача:
-// Что нужно написать вместо  `// ❗❗❗XXX❗❗❗` чтобы на экране отобразились комментарии
-// В качестве ответа укажите написанный вами код
-// ❗Типизацию указывать обязательно
+// При нажатии на кнопку `Add smile` необходимо изменить данные в кеше и добавить к имени переменную
+// smile
+// Результат: http://surl.li/kgmhtn
+// Что нужно написать вместо `// ❗❗❗XXX❗❗❗`, чтобы реализовать данную задачу
+// ❗Изменение стейта должно быть написано мутабельным образом
+// ❗updateRecipe коллбек в качетстве аргумента принимает стейт. Назовите эту переменную state
