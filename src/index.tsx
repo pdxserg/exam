9ea9a8822fbc708f77fb6c917e195235b3cdde9b
SergeyStarchenko <pdxserg@gmail.com>
2025-02-27 08:16:18 -0800

    sprint 5-3-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,81 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider, useDispatch } from "react-redux";
+import React from 'react'
+import ReactDOM from 'react-dom/client';
 
-type User = {
-	id: string;
-	name: string;
-	age: number;
-};
-
-type UsersResponse = {
-	items: User[];
-	totalCount: number;
-};
-
-// Api
-const api = createApi({
-	reducerPath: "api",
-	baseQuery: fetchBaseQuery({ baseUrl: "https://exams-frontend.kimitsu.it-incubator.io/api/" }),
-	endpoints: (builder) => {
-		return {
-			getUsers: builder.query<UsersResponse, void>({
-				query: () => "users",
-			}),
-		};
-	},
-});
-
-const { useGetUsersQuery } = api;
-
-// Users.tsx
-const Users = () => {
-	const { data } = useGetUsersQuery();
-
-	const dispatch = useAppDispatch();
-
-	const addSmileHandler = (id: string) => {
-		const smile = "😁";
-		// ❗❗❗XXX❗❗❗
-	};
-
+export const App = () => {
 	return (
-		<>
-			<h1>Users</h1>
-			{data?.items.map((el) => (
-				<div key={el.id}>
-					name - <b>{el.name}</b>
-					<button onClick={() => addSmileHandler(el.id)}>Add smile</button>
-				</div>
-			))}
-		</>
-	);
-};
+		<div>
+			<h2>Сколько всего веток может быть в репозитории ?</h2>
+			<ul>
+				<li>1 - 2 ветки. master(main) и develop</li>
+				<li>2 - Число веток согласовывается в команде разработчиков и устанавливается в git config</li>
+				<li>3 - Всегда есть ветка master (main), develop может быть по соглашения команды разработчиков. Под каждую фичу
+					создается новая ветка. При этом от ветки с фичей запрещено создавать новые ветки</li>
+				<li>4 - Нет правильного ответа</li>
+			</ul>
+		</div>
+	)
+}
 
-// store.ts
-const store = configureStore({
-	reducer: { [api.reducerPath]: api.reducer },
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
-});
-
-type AppDispatch = typeof store.dispatch;
-const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-
-createRoot(document.getElementById("root")!).render(
-	<Provider store={store}>
-		<Users />
-	</Provider>,
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App/>);
 
 // 📜 Описание:
-// Откройте redux devtools и убедитесь, что данные из запроса хранятся в кеше
-// http://surl.li/veofpd
-// 🪛 Задача:
-// При нажатии на кнопку `Add smile` необходимо изменить данные в кеше и добавить к имени переменную
-// smile
-// Результат: http://surl.li/kgmhtn
-// Что нужно написать вместо `// ❗❗❗XXX❗❗❗`, чтобы реализовать данную задачу
-// ❗Изменение стейта должно быть написано мутабельным образом
-// ❗updateRecipe коллбек в качетстве аргумента принимает стейт. Назовите эту переменную state
+// Сколько всего веток может быть в репозитории ?
+// Может быть несколько вариантов ответа (ответ дайте через пробел).
+// ❗ Ответ будет засчитан как верный, только при полном правильном совпадении.
+// Если указали правильно один вариант (1),
+// а нужно было указать два варианта (1 и 2), то ответ в данном случае будет засчитан как неправильный
+
+// 🖥 Пример ответа: 1
