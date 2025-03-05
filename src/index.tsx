ed106fe828189fbe5b15a377594b2d0667b043bc
SergeyStarchenko <pdxserg@gmail.com>
2025-03-05 12:03:06 -0800

    sprint 0-4

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,4 @@
 <script>
-	let users = ['dimych', 'valera', 'artem', 'katya']
-
-	yo();
-
-	if (users.length > 4) {
-	yo();
-} else {
-
-}
-
-	if (users.length < 2 || window !== null) {
-	yo()
-}
-
-	for (let i=0;i<1;i++) {
-	yo()
-}
-
-	function yo(){}
-
+	let skills = ['css', 'html', 'js'];
+	skills.XXX('react');
 </script>
