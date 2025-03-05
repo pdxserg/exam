969e56fc04cf606bee3aa782459bec1d973de1e7
SergeyStarchenko <pdxserg@gmail.com>
2025-03-05 12:11:45 -0800

    sprint 0-7

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
-<script>
-	function removeWhitespaces(string) {
-	string.split(' ').filter(s => s !== '').join('_')
-}
+<input id="search"/>
+<button id="find">find</button>
 
-	console.log(removeWhitespaces('you are JS developer'))
+<script>
+	const searchInputElement = document.getElementById('search')
+	const huntButtonElement = document.getElementById('find')
+
+	XXX.addEventListener(YYY, () => {
+	console.log(searchInputElement.value)
+})
+
+	/*
+	Что нужно написать внутри скобок вместо XXX и YYY, чтобы при нажатии на кнопку увидеть текст, введённый в input?
+	Ответ дайте через пробел.
+	Пример ответа: firstword secondword
+	*/
+
 </script>
+
